f4d35c5e83e0c2ca3208cfc581c11a0b1617b7ad
Techforge25 <github@techforgeinnovations.com>
2026-01-06 00:47:35 +0500

    Added cors and cookie configurations

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,22 @@
 const isProduction = process.env.NODE_ENV === "production";
 
+const corsOptions = {
+    origin:[process.env.ORIGIN],
+    credentials:true,
+    methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders:["Content-Type", "Authorization"]
+};
+
+const cookieOptions = {
+    httpOnly:true,
+    secure:true,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    signed:true,
+    sameSite:"none"
+};
+
 module.exports = {
-    isProduction
+    isProduction,
+    corsOptions,
+    cookieOptions,
 };

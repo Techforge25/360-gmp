bd062be2a3fdfffc9423f4cbccfc948db9b18a7a
Techforge25 <github@techforgeinnovations.com>
2026-01-06 03:27:54 +0500

    Initialize entry point

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 require("dotenv").config();
+const app = require("./app");
+const { port } = require("./constants");
+
+require("dotenv").config();
+
+// Connect db
+
+// Start app
+app.listen(port, () => console.log(`Server is up adn running`));

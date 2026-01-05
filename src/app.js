2bb84aa5fd8ef6fff0e89deb05f4a2f35701db38
Techforge25 <github@techforgeinnovations.com>
2026-01-06 00:47:18 +0500

    Initialize app

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const { corsOptions } = require("./constants");
+const errorHandler = require("./middlewares/errorHandler");
+
+const app = express();
+
+// Middlewares
+app.use(cors(corsOptions));
+app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.json({ limit: "20kb" }));
+app.use("/public", express.static(path.resolve("public")));
+app.use(compression());
+
+// Error handling middleware
+app.use(errorHandler);
+
+module.exports = app;

9d7b5fcb6f4e50f3f0d34b11f122f51cadb261b8
Techforge25 <github@techforgeinnovations.com>
2026-01-06 01:08:23 +0500

    Setup business profile model

---
+++ src/models/businessProfile.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const BusinessProfileSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    companyName: String,
+    businessType: String,
+    companySize: Number,
+    foundedDate: Date,
+    website: String,
+    description: String,
+    logo: String,
+    banner: String,
+    location: String,
+    isVerified: { type: Boolean, default: false },
+    isLocked: { type: Boolean, default: false }
+}, { timestamps: true });
+
+// Model
+const BusinessProfile = model("BusinessProfile", BusinessProfileSchema);
+
+module.exports = BusinessProfile;

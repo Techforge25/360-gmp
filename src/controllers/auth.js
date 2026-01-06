82348264be89646d6154274649b1ecb9d9425513
Techforge25 <github@techforgeinnovations.com>
2026-01-06 05:15:54 +0500

    Login api created

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,6 @@
+const { cookieOptions } = require("../constants");
+const User = require("../models/users");
+const { generateAccessToken } = require("../utils/accessToken");
 const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
@@ -5,16 +8,35 @@ const validate = require("../utils/validate");
 const { userSignupSchema } = require("../validations/user");
 
 // User signup
-const userSignup = asyncHandler((request, response) => {
-    // const { email, passwordHash } = request.body;
-    // if(!email) throw new ApiError(400, "Email is required");
-    // if(!passwordHash) throw new ApiError(400, "Password is required");
-
+const userSignup = asyncHandler(async (request, response) => {
+    // Validate
     const { email, passwordHash } = validate(userSignupSchema, request.body);
-    console.log(email);
-    console.log(passwordHash);
+    const user = await User.create({ email, passwordHash });
+    if(!user) throw new ApiError(500, "Unable to signup");
 
     return response.status(201).json(new ApiResponse(201, null, "Signup successful"));
 });
 
-module.exports = { userSignup };
+// User login
+const userLogin = asyncHandler(async (request, response) => {
+    const { email, passwordHash } = request.body;
+    if(!email) throw new ApiError(400, "Email is required");
+    if(!passwordHash) throw new ApiError(400, "Password is required");
+
+    // Find user
+    const user = await User.findOne({ email });
+    if(!user) throw new ApiError(400, "Invalid credentials");
+
+    // Match password
+    const isMatched = await user.matchPassword(passwordHash);
+    if(!isMatched) throw new ApiError(400, "Invalid credentials");
+
+    // Generate access token
+    const accessToken = generateAccessToken(user);
+    if(!accessToken) throw new ApiError(500, "Failed to generate access token");
+    return response.status(201)
+    .cookie("accessToken", accessToken, cookieOptions)
+    .json(new ApiResponse(201, null, "Login successful"));
+});
+
+module.exports = { userSignup, userLogin };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
-const { userSignup } = require("../controllers/auth");
+const { userSignup, userLogin } = require("../controllers/auth");
 
 // Router instance
 const authRouter = Router();
 
 // User signup
 authRouter.route("/user/signup").post(userSignup);
+
+// User login
+authRouter.route("/user/login").post(userLogin);
 
 module.exports = authRouter;

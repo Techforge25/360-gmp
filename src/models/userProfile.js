a5201bb501abc7c53f2a8aa31eb3e2ad76d1c26e
Techforge25 <github@techforgeinnovations.com>
2026-01-07 00:42:43 +0500

    Updated user profile model

--- src/models/userProfile.js
+++ src/models/userProfile.js
@@ -2,15 +2,34 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const userProfileSchema = new Schema({
-  userId: { type:Schema.Types.ObjectId, ref: "User", required: true },
-  fullName: String,
-  title: String,
-  phone: String,
-  location: String,
-  bio: String,
-  resumeUrl: String,
-  skills: [String],
-  imageProfile: String
+    // Basic info
+    userId: { type:Schema.Types.ObjectId, ref: "User", required: true },
+    fullName: String,
+    title: String,
+    phone: String,
+    location: String,
+    bio: String,
+    resumeUrl: String,
+    skills: [String],
+    imageProfile: String,
+
+    // For Job Application
+    targetJob: String,
+    employmentType: [String],
+    minSalary: Number,
+    maxSalary: Number,
+
+    // Education fields
+    education: [{
+      institution: String,
+      degree: String,
+      fieldOfStudy: String,
+      startDate: Date,
+      endDate: Date,
+      isCurrent: { type: Boolean, default: false },
+      description: String,
+      grade: String
+    }]
 }, { timestamps: true });
 
 // Model

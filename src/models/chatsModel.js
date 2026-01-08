fe2547de4fa719e2e5c1759d5c9915c716b13717
Techforge25 <github@techforgeinnovations.com>
2026-01-09 03:14:48 +0500

    update chat model with new fields

--- src/models/chatsModel.js
+++ src/models/chatsModel.js
@@ -2,10 +2,13 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const chatSchema = new Schema({
-    senderId:{ type:Schema.Types.ObjectId, ref:"User", required:true },
-    receiverId:{ type:Schema.Types.ObjectId, ref:"User", required:true },
+    senderId: { type:Schema.Types.ObjectId, ref:"User", required:true },
+    receiverId: { type:Schema.Types.ObjectId, ref:"User", required:true },
     conversationId: { type:String, index: true, required:true },
-    message:{ type:String, required:true },
+    message: { type:String, required:true },
+    isRead: { type:Boolean, default:false },
+    messageType: { type:String, enum:["text", "document"], default:"text" },
+    documentFileUrl: { type:String }
 }, { timestamps:true });
 
 // Model

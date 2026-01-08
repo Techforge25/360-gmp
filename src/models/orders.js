037e5c4bc7d1285fae9d509c916cb8d599e9f8b2
Techforge25 <github@techforgeinnovations.com>
2026-01-08 05:07:05 +0500

    Redesign orders schema

--- src/models/orders.js
+++ src/models/orders.js
@@ -1,13 +1,26 @@
 const { Schema, model } = require("mongoose");
 
 // Schema
+// const orderSchema = new Schema({
+//     buyerUserProfileId: { type: Schema.Types.ObjectId, ref: "UserProfile" },
+//     sellerBusinessId: { type: Schema.Types.ObjectId, ref: "BusinessProfile" },
+//     conversationId: { type: Schema.Types.ObjectId, ref: "Conversation" },
+//     customOfferId: { type: Schema.Types.ObjectId, ref: "CustomOffer" },
+//     status: String,
+//     totalAmount: Number
+// }, { timestamps: true });
+
+// Updated Schema
 const orderSchema = new Schema({
     buyerUserProfileId: { type: Schema.Types.ObjectId, ref: "UserProfile" },
-    sellerBusinessId: { type: Schema.Types.ObjectId, ref: "BusinessProfile" },
-    conversationId: { type: Schema.Types.ObjectId, ref: "Conversation" },
-    customOfferId: { type: Schema.Types.ObjectId, ref: "CustomOffer" },
-    status: String,
-    totalAmount: Number
+    totalAmount: { type: String },
+    status: { type: String, default: "pending" },
+    shippingAddress: { type:String },
+    items:[{
+        _id:false,
+        productId: { type: Schema.Types.ObjectId, ref:"Product", required:[true, "Product ID is required"] },
+        quantity: { type: Number, required:[true, "Please specify product quantity"] }
+    }]
 }, { timestamps: true });
 
 // Model

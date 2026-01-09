81bea96f82aaf90a938b02b263599d2ee1318621
Techforge25 <github@techforgeinnovations.com>
2026-01-10 03:37:13 +0500

    update helpers

--- src/utils/getProfiles.js
+++ src/utils/getProfiles.js
@@ -1,17 +1,34 @@
 const BusinessProfile = require("../models/businessProfileSchema");
 const UserProfile = require("../models/userProfile");
+const ApiError = require("./ApiError");
 const asyncHandler = require("./asyncHandler");
 
 // Get user profile
-const getUserProfile = asyncHandler(async (parentUserId) => {
-    const userProfile = await UserProfile.findOne({ userId:parentUserId }).lean();
-    return userProfile;
-});
+const getUserProfile = async (parentUserId) => {
+    if(!parentUserId) throw new ApiError(400, "Parent user ID is missing");
+    try 
+    {
+        const userProfile = await UserProfile.findOne({ userId:parentUserId }).lean();
+        return userProfile;
+    } 
+    catch(error) 
+    {
+        throw error;
+    }
+};
 
 // Get business profile
-const getBusinessProfile = asyncHandler(async (parentUserId) => {
-    const businessProfile = await BusinessProfile.findOne({ ownerUserId:parentUserId }).lean();
-    return businessProfile;
-});
+const getBusinessProfile = async (parentUserId) => {
+    if(!parentUserId) throw new ApiError(400, "Parent user ID is missing");
+    try 
+    {
+        const businessProfile = await BusinessProfile.findOne({ ownerUserId:parentUserId }).lean();
+        return businessProfile;        
+    }
+    catch(error) 
+    {
+        throw error;
+    }
+};
 
 module.exports = { getUserProfile, getBusinessProfile };

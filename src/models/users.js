012ab26191dd77565d9886339d8eda2e97830aae
Techforge25 <github@techforgeinnovations.com>
2026-01-13 10:56:11 +0500

    Refactor password hashing middleware to remove unnecessary next() callback

--- src/models/users.js
+++ src/models/users.js
@@ -13,8 +13,8 @@ const userSchema = new Schema({
 }, { timestamps: true });
 
 // Hash password
-userSchema.pre("save", async function(next) {
-    if(!this.isModified("passwordHash")) return next();
+userSchema.pre("save", async function() {
+    if(!this.isModified("passwordHash")) return;
     try 
     {
         this.passwordHash = await bcrypt.hash(this.passwordHash, 10);

c79ad3714135fed451603f824a0abbbe7614800c
Techforge25 <github@techforgeinnovations.com>
2026-01-14 11:12:55 +0500

    Add customOfferValidator for validating custom offer data

---
+++ src/validations/customOfferValidator.js
@@ -0,0 +1,15 @@
+const joi = requiure("joi");
+
+const customOfferValidator = joi.object({
+    buyerBusinessProfileId: joi.string().required().label("Buyer Business Profile ID"),
+    sellerUserId: joi.string().required().label("Seller User ID"),
+    quantity: joi.number().min(1).required().label("Quantity"),
+    pricePerUnit: joi.number().min(1).required().label("Price Per Unit"),
+    subTotal: joi.number().min(1).required().label("Subtotal"),
+    shippingCost: joi.number().min(1).required().label("Shipping Cost"),
+    method: joi.string().required().label("Shipping Method"),
+    estimatedDelivery: joi.string().required().label("Estimated Delivery"),
+    noteToBuyer: joi.string().allow("").label("Note to Buyer")
+});
+
+module.exports = customOfferValidator;

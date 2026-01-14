37772627f74cb3bff577714718e78de9d1f2bf59
Techforge25 <github@techforgeinnovations.com>
2026-01-14 14:12:13 +0500

    Fix productId field in customOfferValidator and rename method to shippingMethod

--- src/validations/customOfferValidator.js
+++ src/validations/customOfferValidator.js
@@ -2,11 +2,12 @@ const joi = require("joi");
 
 const customOfferValidator = joi.object({
     sellerUserId: joi.string().required().label("Seller User ID"),
+    productId: joi.string().required().label("Product ID"),
     quantity: joi.number().min(1).required().label("Quantity"),
     pricePerUnit: joi.number().min(1).required().label("Price Per Unit"),
     subTotal: joi.number().min(1).required().label("Subtotal"),
     shippingCost: joi.number().min(1).required().label("Shipping Cost"),
-    method: joi.string().required().label("Shipping Method"),
+    shippingMethod: joi.string().required().label("Shipping Method"),
     estimatedDelivery: joi.string().required().label("Estimated Delivery"),
     noteToBuyer: joi.string().allow("").label("Note to Buyer")
 });

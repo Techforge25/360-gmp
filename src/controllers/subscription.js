a7bd7bcd73fd50b5c5523bee0bdd0362e1662511
Techforge25 <github@techforgeinnovations.com>
2026-01-19 16:06:22 +0500

    Send session id for testing

--- src/controllers/subscription.js
+++ src/controllers/subscription.js
@@ -93,7 +93,7 @@ const verifyStripePayment = asyncHandler(async (request, response) => {
         });
 
         if(!subscription) throw new ApiError(500, "Failed to save subscription details in db");
-        return response.status(303).redirect(`${process.env.FRONTEND_URL}/subscription/success`);
+        return response.status(303).redirect(`${process.env.FRONTEND_URL}/subscription/success?session_id=${session_id}`);
         // return response.status(200).json(new ApiResponse(200, null, "Payment verified & subscription activated"));
     } 
     else 

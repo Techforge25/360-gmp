9b83cdc73c3b91a58e05c9cfe0ece23b27862bfc
Techforge25 <github@techforgeinnovations.com>
2026-01-21 11:13:29 +0500

    Remove extra method

--- src/models/products.js
+++ src/models/products.js
@@ -1,4 +1,4 @@
-const { Schema, model, trusted } = require("mongoose");
+const { Schema, model } = require("mongoose");
 const paginate = require("mongoose-paginate-v2");
 const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 

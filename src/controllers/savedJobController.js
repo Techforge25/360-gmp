ea912bf0a9a889ed11c6f69683812a1534da815e
Techforge25 <github@techforgeinnovations.com>
2026-01-22 15:33:25 +0500

    Feature: Saved job, unsave job, fetch my saved jobs

---
+++ src/controllers/savedJobController.js
@@ -0,0 +1,51 @@
+const Job = require("../models/jobsSchema");
+const SavedJob = require("../models/savedJobsModel");
+const ApiError = require("../utils/ApiError");
+const ApiResponse = require("../utils/ApiResponse");
+const asyncHandler = require("../utils/asyncHandler");
+const { isValidObjectId } = require("mongoose");
+
+// Save job
+const saveJob = asyncHandler(async (request, response) => {
+    const userId = request.user._id;
+    const { jobId } = request.params;
+    if(!isValidObjectId(jobId)) throw new ApiError(400, "Invalid Mongodb ID");
+    
+    // Find job
+    const job = await Job.findById(jobId).select("_id").lean();
+    if(!job) throw new ApiError(404, "Job not found");
+
+    // Mark job as save
+    const save = await SavedJob.create({ userId, jobId });
+    if(!save) throw new ApiError(500, "Failed to save job");
+
+    // Response
+    return response.status(201).json(new ApiResponse(201, save, "Job has been saved!"));
+});
+
+// Unsaved job
+const unsavedJob = asyncHandler(async (request, response) => {
+    const userId = request.user._id;
+    const { jobId } = request.params;
+    if(!isValidObjectId(jobId)) throw new ApiError(400, "Invalid Mongodb ID");
+
+    // Unsave
+    const unsave = await SavedJob.findOneAndDelete({ userId, jobId });
+    if(!unsave) throw new ApiError(404, "Saved job not found");
+
+    // Response
+    return response.status(200).json(new ApiResponse(200, unsave, "Job has been unsaved"));
+});
+
+// Fetch my saved jobs
+const fetchMySavedJobs = asyncHandler(async (request, response) => {
+    const userId = request.user._id;
+
+    // Find
+    const savedJobs = await SavedJob.findOne({ userId }).populate("jobId");
+
+    // Response
+    return response.status(200).json(new ApiResponse(200, savedJobs, "My saved jobs have been fetched"));
+});
+
+module.exports = { saveJob, unsavedJob, fetchMySavedJobs };
--- src/routes/savedJobsRoute.js
+++ src/routes/savedJobsRoute.js
@@ -1,6 +1,17 @@
 const { Router } = require("express");
+const { authentication, authorization } = require("../middlewares/auth");
+const { saveJob, fetchMySavedJobs, unsavedJob } = require("../controllers/savedJobController");
 
 // Router instance
 const savedJobRouter = Router();
+
+// Save job / Unsave job
+savedJobRouter.route("/:jobId")
+.post(authentication, authorization(["user"]), saveJob)
+.delete(authentication, authorization(["user"]), unsavedJob);
+
+// Fetch my saved jobs
+savedJobRouter.route("/")
+.get(authentication, authorization(["user"]), fetchMySavedJobs)
 
 module.exports = savedJobRouter;

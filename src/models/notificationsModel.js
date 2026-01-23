8c5956fd9bae5bdc31c65c37903500dcd3846b41
Techforge25 <github@techforgeinnovations.com>
2026-01-23 16:26:29 +0500

    Created a model for user notification

---
+++ src/models/notificationsModel.js
@@ -0,0 +1,13 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const notificationSchema = new Schema({
+    userId:{ type:Schema.Types.ObjectId, ref:"user", required:true },
+    content:{ type:String, required:true },
+    haveSeen:{ type:Boolean, required:true, default:false }
+}, { timestamps:true });
+
+// Model
+const Notification = model("Notification", notificationSchema);
+
+module.exports = Notification;

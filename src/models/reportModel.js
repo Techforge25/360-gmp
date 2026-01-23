55848299f98e465301dc45889e799a4f75da73ce
Techforge25 <github@techforgeinnovations.com>
2026-01-23 10:21:56 +0500

    Redesign report model that can be used on all, job, community, community post and comments

---
+++ src/models/reportModel.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const reportSchema = new Schema({
+    // Reference
+    reporterId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    reportedModel: { type: String, required:true, enum: ["Job", "Community", "CommunityPost"] },
+    reportedContentId: { type: Schema.Types.ObjectId, required:true },
+    reportedCommentId: { type: Schema.Types.ObjectId, default: null },
+
+    // Details
+    subject: { type: String, required: true, trim: true },
+    category: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    evidences: [{ type: String }],
+    status: { type: String, enum: ["pending", "reviewed", "resolved", "rejected"], default: "pending" }
+}, { timestamps: true });
+
+// Model
+const Report = model("Report", reportSchema);
+
+module.exports = Report;

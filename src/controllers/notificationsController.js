67fed7cf4b38e1711296d910bfcb841c359f48bd
Techforge25 <github@techforgeinnovations.com>
2026-01-26 15:40:23 +0500

    Adjusted few edge cases

--- src/controllers/notificationsController.js
+++ src/controllers/notificationsController.js
@@ -32,14 +32,18 @@ const fetchMyNotifications = asyncHandler(async (request, response) => {
 const markAllAsRead = asyncHandler(async (request, response) => {
     const userId = request.user._id;
 
-    // Update
-    const notifications = await Notification.updateMany(
+    // Find notifications and check if they are already read
+    const notifications = await Notification.find({ userId, haveSeen:false }).lean();
+    if(!notifications.length) return response.status(200).json(new ApiResponse(200, notifications, "All notifications have already been read"));
+
+    // Mark all as read
+    const markNotifications = await Notification.updateMany(
         { userId, haveSeen:false }, 
         { $set:{ haveSeen:true } }
     );
 
     // Response
-    return response.status(200).json(new ApiResponse(200, notifications, "All notifications have been marked as read"));
+    return response.status(200).json(new ApiResponse(200, markNotifications, "All notifications have been marked as read"));
 });
 
 module.exports = { createNotification, fetchMyNotifications, markAllAsRead };

b1765360ab2a57c492e90694754ae2daa0b570be
Techforge25 <github@techforgeinnovations.com>
2026-01-27 16:14:18 +0500

    Update validation schema

--- src/validations/supportEmailValidator.js
+++ src/validations/supportEmailValidator.js
@@ -1,7 +1,6 @@
 const joi = require("joi");
 
 const supportEmailValidationSchema = joi.object({
-    userId: joi.string().required().label("User ID"),
     subject: joi.string().max(50).min(5).required().label("Subject"),
     category: joi.string().required().label("Category"),
     description: joi.string().required().label("Description"),

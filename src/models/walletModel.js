a4ae26d516b9b6d4bd021270d9af7825e3073647
Techforge25 <github@techforgeinnovations.com>
2026-01-29 11:14:58 +0500

    Normalize wallet schema

--- src/models/walletModel.js
+++ src/models/walletModel.js
@@ -1,15 +1,15 @@
 const { Schema, model } = require("mongoose");
 
 // Schema
-
 const walletSchema = new Schema({
-    businessId: { type: Schema.Types.ObjectId, ref: "BusinessProfile", required: true, unique: true },
+    businessId: { type: Schema.Types.ObjectId, ref:"BusinessProfile", required:true, unique:true },
     pendingBalance: { type: Number, default: 0 },    
     availableBalance: { type: Number, default: 0 }, 
     totalEarned: { type: Number, default: 0 },       
     currency: { type: String, default: "USD" }
 }, { timestamps: true });
 
+// Model
 const Wallet = model("Wallet", walletSchema);
 
 module.exports = Wallet;

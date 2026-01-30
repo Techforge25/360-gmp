2deb6f8a6d17617df9ba21f56957d84400c8d725
Techforge25 <github@techforgeinnovations.com>
2026-01-30 10:24:37 +0500

    Added withdrawal amount in valivation schema

--- src/validations/withdrawFundValidator.js
+++ src/validations/withdrawFundValidator.js
@@ -2,6 +2,7 @@ const joi = require("joi");
 
 const withdrawFundsValidationSchema = joi.object({
     ownerModel: joi.string().trim().valid("BusinessProfile", "UserProfile").required().label("Owner model"),
+    withdrawalAmount: joi.number().min(200).max(1000).required().label("Withdrawal amount")
 });
 
 module.exports = withdrawFundsValidationSchema;

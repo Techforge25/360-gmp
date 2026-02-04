a547e71eb59eef13872d21a943045c523dda0509
Techforge25 <github@techforgeinnovations.com>
2026-02-04 13:10:52 +0500

    Created a model for work experience

---
+++ src/models/workExperienceModel.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const workExperienceSchema = new Schema({
+    userProfileId: { type:Schema.Types.ObjectId, ref:"UserProfile", required:true },
+    jobTitle: { type:String, trim:true, required:true },
+    employementType: [{ type:String, trim:true }],
+    companyName: { type:String, trim:true, required:true },
+    startDate: { type:Date, required:true },
+    endDate: { type:Date },
+    location: { type:String, trim:true, required:true },
+    description: { type:String, trim:true },
+    isCurrentlyWorking: { type:Boolean, default:false },
+}, { timestamps:true });
+
+// Model
+const WorkExperience = model("WorkExperience", workExperienceSchema);
+
+module.exports = WorkExperience;

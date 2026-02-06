0560b6b86218cdfbfd27456109e62b8fdb6beab4
Techforge25 <github@techforgeinnovations.com>
2026-02-06 16:05:36 +0500

    Fix typos

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -2,7 +2,7 @@ const Notification = require("../models/notificationsModel");
 
 // Send notification helper
 const sendNotification = async ({ userOwnerId, title, content, type = "account", io }) => {
-   const notification = await Notification.create({ userOwnerId, title, content, type });
+   const notification = await Notification.create({ userId: userOwnerId, title, content, type });
    io.to(`user:${userOwnerId}`).emit("notification", notification);
 };
 

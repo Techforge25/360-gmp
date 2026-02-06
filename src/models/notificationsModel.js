4bc0ddd83f089fd8320bf4894c61c8b21dae24f4
Techforge25 <github@techforgeinnovations.com>
2026-02-06 10:42:55 +0500

    Update notification model

--- src/models/notificationsModel.js
+++ src/models/notificationsModel.js
@@ -2,10 +2,11 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const notificationSchema = new Schema({
-    userId:{ type:Schema.Types.ObjectId, ref:"user", required:true },
-    title:{ type:String, trim:true, required:true },
-    content:{ type:String, trim:true, required:true },
-    haveSeen:{ type:Boolean, required:true, default:false }
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    title: { type: String, trim: true, required: true },
+    content: { type: String, trim: true, required: true },
+    type: { type: String, enum: ["system", "account", "payment", "general", "security"], default:"system" },
+    haveSeen: { type: Boolean, default: false }
 }, { timestamps:true });
 
 // Model

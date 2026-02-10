42b6ac118eeac11b90e574bdccda7dae1660c403
Techforge25 <github@techforgeinnovations.com>
2026-02-10 12:39:44 +0500

    Clean and sclae chat controller

--- src/controllers/chatsController.js
+++ src/controllers/chatsController.js
@@ -3,38 +3,21 @@ const Chat = require("../models/chatsModel");
 const CustomOffer = require("../models/customOfferModel");
 const Product = require("../models/products");
 const UserProfile = require("../models/userProfile");
-const User = require("../models/users");
 const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
 const generateConversationId = require("../utils/generateConversationId");
+const validate = require("../utils/validate");
+const { privateMessageValidationSchema } = require("../validations/chatValidator");
 
 // Send private message
 const sendPrivateMessage = asyncHandler(async (request, response) => {
-    const { senderId, senderModel, receiverId, receiverModel, message, messageType = "text", fileUrl = null } = request.body || {};
-
-    // Validate sender
-    if(!senderId) throw new ApiError(400, "Sender ID is missing");
-    if(!senderModel) throw new ApiError(400, "Please specify sender model");
-    if(senderModel !== "UserProfile" && senderModel !== "BusinessProfile") throw new ApiError(400, "Invalid sender model");
-
-    // Validate receiver
-    if(!receiverId) throw new ApiError(400, "Receiver ID is missing");
-    if(!receiverModel) throw new ApiError(400, "Please specify receiver model");
-    if(receiverModel !== "UserProfile" && receiverModel !== "BusinessProfile") throw new ApiError(400, "Invalid reciever model");
+    // Validate payload
+    const { senderId, senderModel, receiverId, receiverModel, message, messageType = "text", 
+    fileUrl = null, customOfferDetails = null } = validate(privateMessageValidationSchema, request.body) || {};
 
     // Restrict sending message to self
     if(receiverId === senderId) throw new ApiError(400, "You cannot send a message to yourself");
-
-    // Validate message
-    if(!message) throw new ApiError(400, "Message is required");
-    if(messageType.toLowerCase() !== "text" && messageType.toLowerCase() !== "file" && messageType.toLowerCase() !== "customOffer") 
-    {
-        throw new ApiError(400, "Invalid message type");
-    }
-
-    // If the message contains any file
-    if(messageType.toLowerCase() === "file" && !fileUrl) throw new ApiError(400, "File url is missing");
 
     // Restrict profile from sending messages to same level profile
     if(senderModel === receiverModel) throw new ApiError(400, "You cannot send a message"); 
@@ -42,18 +25,17 @@ const sendPrivateMessage = asyncHandler(async (request, response) => {
     // Generate unique id for conversation thread
     const conversationId = generateConversationId(senderId, receiverId);
     
-    let customOfferContent = null;
+    let customOfferPayload = null;
 
     // Custom offer
-    if(messageType.toLowerCase() === "customOffer")
+    if(messageType === "customOffer")
     {
         // Only business profile can create custom offer to user profile
         if(senderModel !== "BusinessProfile") throw new ApiError(400, "You are not allowed to create a custom offer");
         if(receiverModel !== "UserProfile") throw new ApiError(400, "You are not allowed to request a custom offer to a business profile");
 
-        // Extract custom offer details from message
-        const { productId, quantity, pricePerUnit, subTotal, shippingCost, shippingMethod,
-        estimatedDelivery, noteToBuyer } = request.body;
+        // Extract custom offer details
+        const { productId, quantity, pricePerUnit, subTotal } = customOfferDetails || {};
 
         // Validate sub total
         const calcultatedSubTotal = Number(quantity) * Number(pricePerUnit);
@@ -72,22 +54,15 @@ const sendPrivateMessage = asyncHandler(async (request, response) => {
         if(!businessProfile) throw new ApiError(404, "Sender business profile not found");
         if(!userProfile) throw new ApiError(404, "Receiver user profile not found");
 
-        // Create custom offer message content
-        customOfferContent = {
+        // Prepare custom offer payload
+        customOfferPayload = {
             sellerBusinessProfileId: senderId,
             buyerUserProfileId: receiverId,
-            productId: productId,
-            quantity: Number(quantity),
-            pricePerUnit: Number(pricePerUnit),
-            subTotal: Number(subTotal),
-            shippingCost: Number(shippingCost),
-            shippingMethod,
-            estimatedDelivery,
-            noteToBuyer
+            ...customOfferDetails
         };
 
         // Save to custom offer collection
-        const customOffer = await CustomOffer.create(customOfferContent);
+        const customOffer = await CustomOffer.create(customOfferPayload);
         if(!customOffer) throw new ApiError(500, "Failed to create custom offer");
     }
 
@@ -103,7 +78,7 @@ const sendPrivateMessage = asyncHandler(async (request, response) => {
     if(!chat) throw new ApiError(400, "Failed to send a message");
 
     // Response
-    return response.status(201).json(new ApiResponse(201, { chat, customOfferContent }, "Message has been sent"));
+    return response.status(201).json(new ApiResponse(201, { chat, customOfferPayload }, "Message has been sent"));
 });
 
 // Fetch private messages

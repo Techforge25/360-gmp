80751959d2b1a304a40b9e12bc65b0fe02714a3f
Techforge25 <github@techforgeinnovations.com>
2026-02-10 10:05:06 +0500

    Added system to notification list

--- src/models/notificationsModel.js
+++ src/models/notificationsModel.js
@@ -5,7 +5,7 @@ const notificationSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true, index: true },
     title: { type: String, trim: true, required: true },
     content: { type: String, trim: true, required: true },
-    type: { type: String, enum: ["account", "order", "payment", "general", "security"], default:"account" },
+    type: { type: String, enum: ["account", "order", "payment", "general", "security", "system"], default:"account" },
     haveSeen: { type: Boolean, default: false }
 }, { timestamps:true });
 

ca8cab90eb499e295550cc3b4cade215e9bc8c44
Techforge25 <github@techforgeinnovations.com>
2026-02-11 12:34:39 +0500

    Added pagination plugin

--- src/models/galleryModel.js
+++ src/models/galleryModel.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const paginate = require("mongoose-paginate-v2");
 
 // Schema
 const gallerySchema = new Schema({
@@ -7,6 +8,9 @@ const gallerySchema = new Schema({
     description: { type: String, default:null },
     images: [{ type: String }]
 }, { timestamps: true });
+
+// Inject pagination plugin
+gallerySchema.plugin(paginate);
 
 // Model
 const Gallery = model("Gallery", gallerySchema);

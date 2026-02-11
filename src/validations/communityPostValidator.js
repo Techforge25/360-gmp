81d01ae1f0a30a730f2802830a806e1a964ff118
Techforge25 <github@techforgeinnovations.com>
2026-02-11 10:13:44 +0500

    Updated validation schema for community post

--- src/validations/communityPostValidator.js
+++ src/validations/communityPostValidator.js
@@ -2,9 +2,13 @@ const joi = require("joi");
 
 // Create Post schema
 const createPostSchema = joi.object({
+    // References
     communityId: joi.string().required().label("Community ID"),
-    content: joi.string().min(1).max(5000).required().trim().label("Post Content"),
-    type: joi.string().valid("post", "document", "event", "poll", "file").default("post").label("Post Type"),
+    type: joi.string().valid("post", "file", "document", "event", "poll").default("post").label("Post Type"),
+
+    // Simple content for post
+    content: joi.string().min(1).max(5000).trim()
+    .when('type', { is: 'post', then: joi.required(), otherwise: joi.optional() }).label("Post Content"),
 
     // File
     file: {
@@ -33,6 +37,7 @@ const createPostSchema = joi.object({
         duration: joi.date().label("Poll Duration")
     }).when('type', { is: 'poll', then: joi.required(), otherwise: joi.optional() }).label("Poll Details"),
 
+    // Post Sharing
     shareTo: joi.string().valid("public", "private").default("public").label("Share To"),
     tags: joi.string().trim().allow("", null).optional().label("Tags"),
 
@@ -42,8 +47,36 @@ const createPostSchema = joi.object({
 
 // Update Post schema
 const updatePostSchema = joi.object({
-    content: joi.string().min(1).max(5000).trim().label("Post Content"),
-    images: joi.array().items(joi.string().trim())
+    // Post Type
+    type: joi.string().valid("post", "file", "document", "event", "poll").default("post").label("Post Type"),
+
+    // Simple content for post
+    content: joi.string().min(1).max(5000).trim().label("Post Content")
+    .when('type', { is: 'post', then: joi.required(), otherwise: joi.optional() }).label("Post Content"),
+
+    // Event Details
+    event: joi.object({
+        name: joi.string().trim().label("Event Name"),
+        description: joi.string().trim().label("Event Description"),
+        date: joi.date().label("Event Date"),
+        location: joi.string().trim().label("Event Location")
+    }).when('type', { is: 'event', then: joi.required(), otherwise: joi.optional() }).label("Event Details"),
+
+    // Poll Details
+    poll: joi.object({
+        question: joi.string().trim().required().label("Poll Question"),
+        options: joi.array().items(joi.object({
+            option: joi.string().trim().label("Poll Option")
+        })).min(2).max(10).label("Poll Options"),
+        duration: joi.date().label("Poll Duration")
+    }).when('type', { is: 'poll', then: joi.required(), otherwise: joi.optional() }).label("Poll Details"),
+
+    // Post Sharing
+    shareTo: joi.string().valid("public", "private").default("public").label("Share To"),
+    tags: joi.string().trim().allow("", null).optional().label("Tags"),
+
+    images: joi.array().items(joi.string().trim()).default([]),
+    docId: joi.string().trim().allow("", null)    
 });
 
 // Like/Unlike Post schema

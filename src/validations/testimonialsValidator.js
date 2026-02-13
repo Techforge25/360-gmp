48fcc7352cde5c82154457f30947bef33a838a42
Techforge25 <github@techforgeinnovations.com>
2026-02-13 13:11:42 +0500

    Update validation schema for rating

--- src/validations/testimonialsValidator.js
+++ src/validations/testimonialsValidator.js
@@ -3,8 +3,8 @@ const joi = require("joi");
 // Validation schema for creating a testimonial
 const createTestimonialValidationSchema = joi.object({
     rating: joi.number().integer().min(1).max(5).required().label("Rating (1-5)"),
-    title: joi.string().max(50).optional().label("Title"),
-    description: joi.string().trim().max(1000).required().label("Description")
+    title: joi.string().trim().max(50).optional().label("Testimonial Title"),
+    description: joi.string().trim().max(1000).required().label("Testimonial Description")
 });
 
 module.exports = { createTestimonialValidationSchema };

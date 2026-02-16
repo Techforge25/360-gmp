809c7c8414fd592a97e5974c608cb84792206c82
Techforge25 <github@techforgeinnovations.com>
2026-02-16 11:57:38 +0500

    Make sendNotification helper function dynamic for handling public and privare notifications

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -1,9 +1,31 @@
 const Notification = require("../models/notificationsModel");
+const ApiError = require("./ApiError");
 
 // Send notification helper
-const sendNotification = async ({ userOwnerId, title, content, type = "account", io }) => {
-   const notification = await Notification.create({ userId: userOwnerId, title, content, type });
-   io.to(`user:${userOwnerId}`).emit("notification", notification);
+const sendNotification = async ({ userOwnerId = null, title, content, type = "account", io }) => {
+   try 
+   {
+      // Save to db
+      const notification = await Notification.create({ userId: userOwnerId, title, content, type });
+
+      // Enforce user owner id for non-public notification
+      if(type !== "public" && !userOwnerId) throw new ApiError(400, "User owner ID is required for non-public notifications");
+
+      if(type.toLowerCase() === "public")
+      {
+         // Public notification
+         io.emit("notification", notification);
+      }
+      else
+      {
+         // Private notification
+         io.to(`user:${userOwnerId}`).emit("notification", notification);
+      }      
+   } 
+   catch(error) 
+   {
+      throw new ApiError(500, "Failed to save notification");
+   }
 };
 
 module.exports = sendNotification;

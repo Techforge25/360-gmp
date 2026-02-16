849fff6e5634d57ad5fccc7d53f73850bb426f0b
Techforge25 <github@techforgeinnovations.com>
2026-02-16 11:56:55 +0500

    Enhance notification model

--- src/models/notificationsModel.js
+++ src/models/notificationsModel.js
@@ -1,13 +1,17 @@
 const { Schema, model } = require("mongoose");
+const paginate = require("mongoose-paginate-v2");
 
 // Schema
 const notificationSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    userId: { type: Schema.Types.ObjectId, ref: "User", index: true },
     title: { type: String, trim: true, required: true },
     content: { type: String, trim: true, required: true },
-    type: { type: String, enum: ["account", "order", "payment", "general", "security", "system"], default:"account" },
+    type: { type: String, enum: ["account", "order", "payment", "general", "security", "system", "public"], default:"account" },
     haveSeen: { type: Boolean, default: false }
 }, { timestamps:true });
+
+// Inject pagination plugin
+notificationSchema.plugin(paginate);
 
 // Model
 const Notification = model("Notification", notificationSchema);

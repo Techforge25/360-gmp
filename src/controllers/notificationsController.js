73caca146e4f6c137fbe6cd5f651516a529bcb9e
Techforge25 <github@techforgeinnovations.com>
2026-02-16 11:57:54 +0500

    Retrieve both private and public notifications

--- src/controllers/notificationsController.js
+++ src/controllers/notificationsController.js
@@ -1,28 +1,22 @@
+const { emptyList } = require("../constants");
 const Notification = require("../models/notificationsModel");
-const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
-const { isValidObjectId } = require("mongoose");
-
-// Helper to create notification
-const createNotification = async (userId, title, content) => {
-    // Validate Mongodb ID and notification content
-    if(!isValidObjectId(userId)) throw new ApiError(400, "Invalid Object ID");
-    if(!title) throw new ApiError(400, "Notification title is required");
-    if(!content) throw new ApiError(400, "Notification content is required");
-
-    // Add notification
-    const notification = await Notification.create({ userId, title, content });
-    if(!notification) throw new ApiError(500, "Failed to create notification");
-
-    // Response
-    return true;
-};
 
 // Fetch my notifications
 const fetchMyNotifications = asyncHandler(async (request, response) => {
     const userId = request.user._id;
-    const notifications = await Notification.find({ userId }).sort({ createdAt:-1 }).lean();
+
+    // Options
+    const options = {
+        page: Number(request.query.page) || 1,
+        limit: Number(request.query.limit) || 10,
+        sort: { createdAt:-1 }
+    };
+
+    // Retrieve notifications
+    const notifications = await Notification.paginate({ $or:[{ userId }, { type:"public" }] }, options);
+    if(!notifications.docs?.length) return response.status(200).json(new ApiResponse(200, emptyList, "You don't have any notification"));
 
     // Response
     return response.status(200).json(new ApiResponse(200, notifications, "Notifications have been fetched"));
@@ -46,4 +40,4 @@ const markAllAsRead = asyncHandler(async (request, response) => {
     return response.status(200).json(new ApiResponse(200, markNotifications, "All notifications have been marked as read"));
 });
 
-module.exports = { createNotification, fetchMyNotifications, markAllAsRead };
+module.exports = { fetchMyNotifications, markAllAsRead };

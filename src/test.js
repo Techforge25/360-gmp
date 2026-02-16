139484e00ca06de5053a89006e17c54b4e764fe3
Techforge25 <github@techforgeinnovations.com>
2026-02-16 15:45:22 +0500

    Wrote test cases in file flow to implement seen message functionality in real time

--- src/test.js
+++ src/test.js
@@ -24,7 +24,18 @@ socket.on("connect_error", (err) => console.log("Connection Error:", err.message
 socket.on("disconnect", () => console.log("Disconnected from server"));
 
 // Listen for notification
-socket.on("notification", (data) => console.log("Notification recieved", data));
+// socket.on("notification", (data) => console.log("Notification recieved", data));
+
+// Seen message functionality
+// const messageId = '698ae6250f47b8d6ca276784';
+// const senderId = '698058aab04c746c04d0d61d';
+// const receiverId = '69805851b04c746c04d0d589';
+
+// // Emit once read the message
+// socket.emit("read-message", ({ messageId, senderId, receiverId }));
+
+// // Listen-back for read message
+// socket.on("read-message", ({ isRead }) => console.log("Client received read flag from server:", isRead));
 
 // Listen for messages
 // socket.on("message", (data) => console.log("Message recieved", data));

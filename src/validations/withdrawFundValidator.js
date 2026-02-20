3bf5102f97d0612002e42ae8f8f868ba0dd5309c
Techforge25 <github@techforgeinnovations.com>
2026-02-20 07:33:35 +0500

    Set minimum amount from 200 to 50 for withdrawal

--- src/validations/withdrawFundValidator.js
+++ src/validations/withdrawFundValidator.js
@@ -2,8 +2,8 @@ const joi = require("joi");
 
 const withdrawFundsValidationSchema = joi.object({
     ownerModel: joi.string().trim().valid("BusinessProfile", "UserProfile").required().label("Owner model"),
-    withdrawalAmount: joi.number().min(200).max(1000).required().label("Withdrawal amount").messages({
-        "number.min": "{{#label}} cannot be less than 200",
+    withdrawalAmount: joi.number().min(50).max(1000).required().label("Withdrawal amount").messages({
+        "number.min": "{{#label}} cannot be less than 50",
         "number.max": "{{#label}} cannot be more than 1000"
     })
 });

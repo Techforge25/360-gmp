62e5bd665d921a7310c980337ecd1b2c5d22683c
Techforge25 <github@techforgeinnovations.com>
2026-02-26 01:02:53 +0500

    Registered dashboard router

--- src/app.js
+++ src/app.js
@@ -81,9 +81,11 @@ app.use("/api/v1/dispute", disputeRouter);
 
 // Import Admin Routes
 const adminAuthRouter = require("./routes/admin/adminAuthRoute");
+const dashboardRouter = require("./routes/admin/dashboardRoute");
 
 // Registered Admin Routes
 app.use("/api/v1/admin/auth", adminAuthRouter);
+app.use("/api/v1/admin/dashboard", dashboardRouter);
 
 // API status route
 app.get("/", (request, response) => response.send(`Server is up and running at port ${port}`));

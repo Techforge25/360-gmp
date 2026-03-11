2a479ed66507e10c6b8e89736971c961d7e2d17b
Techforge25 <github@techforgeinnovations.com>
2026-03-11 23:50:02 +0500

    Refactor market place controllers

--- src/controllers/admin/marketplaceController.js
+++ src/controllers/admin/marketplaceController.js
@@ -1,25 +1,47 @@
 const { emptyList } = require("../../constants");
 const EscrowTransaction = require("../../models/escrowTrasanction");
+const Product = require("../../models/products");
 const ApiResponse = require("../../utils/ApiResponse");
 const asyncHandler = require("../../utils/asyncHandler");
 
-// Fetch total sales
-const fetchTotalSales = asyncHandler(async (request ,response) => {
+// Sum total sales
+const sumTotalSales = asyncHandler(async (request ,response) => {
     //Fetch
     const totalSales = await EscrowTransaction.aggregate([
         {
             $group:{
                 _id:null,
-                totalAmount:{ $sum:1 }
+                totalAmount: { $sum: "$totalAmount" }
             }
-        },
+        }
     ]);
 
     // Prepare payload
-    const payload = { totalSales: totalSales[0]?.totalAmount || 0 };
+    const payload = { totalSales: Number(totalSales[0]?.totalAmount) || 0 };
 
     // Response
-    return response.status(200).json(new ApiResponse(200, payload, "Total sales have been fetched"));
+    return response.status(200).json(new ApiResponse(200, payload, "Sum of total sales have been fetched"));
 });
 
-module.exports = { fetchTotalSales };
+// Sum pending products
+const sumPendingProducts = asyncHandler(async (request, response) => {
+    const pendingProducts = await Product.aggregate([
+        { $match:{ status:"pending" } },
+
+        // Sum
+        {
+            $group: {
+                _id:null,
+                totalPendingProducts:{ $sum:1 }
+            }
+        }
+    ]);
+
+    // Prepare payload
+    const payload = { totalPendingProducts: Number(pendingProducts[0]?.totalPendingProducts) || 0 };
+
+    // Response
+    return response.status(200).json(new ApiResponse(200, payload, "Sum of total pending products have been fetched"));    
+});
+
+module.exports = { sumTotalSales, sumPendingProducts };
--- src/routes/admin/marketplaceRoute.js
+++ src/routes/admin/marketplaceRoute.js
@@ -1,12 +1,16 @@
 const { Router } = require("express");
 const { adminAuthentication } = require("../../middlewares/adminAuth");
-const { fetchTotalSales } = require("../../controllers/admin/marketPlaceController");
+const { sumTotalSales, sumPendingProducts } = require("../../controllers/admin/marketPlaceController");
 
 // Router instance
 const marketPlaceRouter = Router();
 
-// Fetch total sales
+// Sum total sales
 marketPlaceRouter.route("/totalSales")
-.get(adminAuthentication, fetchTotalSales);
+.get(adminAuthentication, sumTotalSales);
+
+// Sum pending products
+marketPlaceRouter.route("/pendingProducts")
+.get(adminAuthentication, sumPendingProducts);
 
 module.exports = marketPlaceRouter;

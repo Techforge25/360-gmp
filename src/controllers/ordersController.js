8bf5b5c98de2bd946ee7ed0094069c5fd39556dc
Techforge25 <github@techforgeinnovations.com>
2026-03-18 21:14:13 +0500

    Change redirect url

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -299,7 +299,7 @@ const verifyStripePaymentForOrders = asyncHandler(async (request, response) => {
         io.to(String(businessProfile._id)).emit("order-creation", order);
 
         // Response
-        return response.status(303).redirect(`${frontendUrl}/dashboard/user/checkout/${order._id}`);
+        return response.status(303).redirect(`${frontendUrl}/dashboard/user/checkout/payment-confirmation/${order._id}`);
     } 
     catch(error) 
     {

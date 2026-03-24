f2bbb79c289fd6eaebb35fab2016560b6875e995
Techforge25 <github@techforgeinnovations.com>
2026-03-24 14:14:46 +0500

    Update validator for job application schema

--- src/validations/jobApplication.js
+++ src/validations/jobApplication.js
@@ -1,11 +1,11 @@
 const Joi = require("joi");
 
 const createJobApplicationSchema = Joi.object({
-    resumeUrl: Joi.string().required(),
-    portfolioLink: Joi.string().optional(),
-    yearsOfExperience: Joi.number().min(0).optional(),
-    immediateJoiningStatus: Joi.string().valid("Yes", "No").optional(),
-    expectedSalary: Joi.string().optional()
+    resumeUrl: Joi.string().trim().uri().required().label("Resume"),
+    portfolioLink: Joi.string().trim().optional().allow(null, "").uri().label("Portfolio link"),
+    yearsOfExperience: Joi.number().min(0).optional().label("Years of experience"),
+    immediateJoiningStatus: Joi.string().valid("Yes", "No").optional().label("Immediate joining status"),
+    expectedSalary: Joi.number().integer().positive().optional().label("Expected salary")
 });
 
 module.exports = { createJobApplicationSchema };

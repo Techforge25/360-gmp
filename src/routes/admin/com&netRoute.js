39875f14aafc8305060f8b1075a3b49de79fb966
Techforge25 <github@techforgeinnovations.com>
2026-03-27 15:45:27 +0500

    Create API to sum total communities

--- src/routes/admin/com&netRoute.js
+++ src/routes/admin/com&netRoute.js
@@ -1,6 +1,12 @@
 const { Router } = require("express");
+const { adminAuthentication } = require("../../middlewares/adminAuth");
+const { totalCommunities } = require("../../controllers/admin/com&netController");
 
 // Router instance
 const comAndNetRouter = Router();
+
+// Count total communities
+comAndNetRouter.route("/totalCommunities")
+.get(adminAuthentication, totalCommunities);
 
 module.exports = comAndNetRouter;

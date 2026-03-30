992c05c58c79037f519927e7ca23892f2f30bddf
Techforge25 <github@techforgeinnovations.com>
2026-03-30 10:08:05 +0500

    Create subscription history model

---
+++ src/models/subscriptionHistoryModel.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+const aggregatePaginate = require("mongoose-aggregate-paginate-v2")
+
+// Schema
+const subscriptionHistorySchema = new Schema({
+    // References
+    userId: { type:Schema.Types.ObjectId, ref:"User" },
+    planId: { type:Schema.Types.ObjectId, ref:"Plan" },
+
+    // Other info
+    invoiceId:{ type:String, trim:true, default:null },
+    status: { type:String, enum: ["pending", "paid", "failed"], default: "pending" }
+}, { timestamps:true });
+
+// Add pagination plugin
+subscriptionHistorySchema.plugin(aggregatePaginate);
+
+const SubscriptionHistory = model("SubscriptionHistory", subscriptionHistorySchema);
+
+module.exports = SubscriptionHistory;

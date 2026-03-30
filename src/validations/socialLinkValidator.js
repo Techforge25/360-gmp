353d8e55f6053ed0c6722b5e0e91965d1b001743
Techforge25 <github@techforgeinnovations.com>
2026-03-30 13:00:26 +0500

    Validate platform name

--- src/validations/socialLinkValidator.js
+++ src/validations/socialLinkValidator.js
@@ -2,7 +2,7 @@ const joi = require("joi");
 
 // Social link validation schema
 const socialLinkValidationSchema = joi.object({
-    platformName: joi.string().required().label("Platform Name"),
+    platformName: joi.string().trim().max(100).required().label("Platform Name"),
     url: joi.string().uri().required().label("URL")
 });
 

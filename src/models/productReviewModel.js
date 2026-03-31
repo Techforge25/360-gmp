e7d3307336b7a706ccfd82d8a25bdb5d1cc7d7a1
Techforge25 <github@techforgeinnovations.com>
2026-03-31 15:50:14 +0500

    Enable index

--- src/models/productReviewModel.js
+++ src/models/productReviewModel.js
@@ -4,12 +4,12 @@ const { Schema, model } = require("mongoose");
 const productReviewSchema = new Schema({
     // References
     userProfileId: { type:Schema.Types.ObjectId, ref:"UserProfile", required:true },
-    productId: { type:Schema.Types.ObjectId, ref:"Product", required:true },
+    productId: { type:Schema.Types.ObjectId, ref:"Product", required:true, index:true },
 
     // Review info
     rating: { type:Number, min:1, max:5, required:true },
-    comment:{ type:String, trim:true, required:true },
-    images:[{ type:String, trim:true }]
+    comment: { type:String, trim:true, required:true },
+    images: [{ type:String, trim:true }]
 }, { timestamps:true });
 
 // Model

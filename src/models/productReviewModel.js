fee2521fb4987f3cac75b7e41aaa2f7c60f3ac6d
Techforge25 <github@techforgeinnovations.com>
2026-03-31 12:13:40 +0500

    Create: Product review model

---
+++ src/models/productReviewModel.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+// Schema
+const productReviewSchema = new Schema({
+    // References
+    userId: { type:Schema.Types.ObjectId, ref:"User", required:true },
+    productId: { type:Schema.Types.ObjectId, ref:"Product", required:true },
+
+    // Review info
+    rating: { type:Number, min:1, max:5, required:true },
+    comment:{ type:String, trim:true, required:true },
+    images:[{ type:String, trim:true }]
+});
+
+// Model
+const ProductReview = model("ProductReview", productReviewSchema);
+
+module.exports = ProductReview;

7d615a002d4c77b033741a5018a817e253f09710
Techforge25 <github@techforgeinnovations.com>
2026-04-02 13:52:54 +0500

    Update reset password validator

--- src/validations/resetPasswordValidator.js
+++ src/validations/resetPasswordValidator.js
@@ -1,9 +1,15 @@
 const joi = require("joi");
 
+const passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&^#()[\\]{}|\\\\<>+=._-])[A-Za-z\\d@$!%*?&^#()[\\]{}|\\\\<>+=._-]+$";
+
 // Reset password schema
-const resetPasswordSchema = joi.object({
-    newPassword: joi.string().min(6).max(20).required().label("New Password"),
-    confirmPassword: joi.string().valid(joi.ref("newPassword")).required().label("Confirm Password")
+const resetPasswordSchema = joi.object({ 
+    newPassword: joi.string().min(8).max(128)
+    .pattern(new RegExp(passwordPattern)).required().messages({
+        "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+        "string.min": "Password must be at least 8 characters long."
+    }).label("New password"),    
+    confirmPassword: joi.string().valid(joi.ref("newPassword")).required().label("Confirm password")
 });
 
 module.exports = resetPasswordSchema;

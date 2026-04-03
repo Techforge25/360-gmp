79c5cb47d48b0b36f8493937a7e97edf83d20109
Techforge25 <github@techforgeinnovations.com>
2026-04-03 11:56:40 +0500

    Enable index

--- src/models/subscription.js
+++ src/models/subscription.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const subscriptionSchema = new Schema({
-    userId: { type:Schema.Types.ObjectId, ref:"User" },
+    userId: { type:Schema.Types.ObjectId, ref:"User", index:true },
     planId: { type:Schema.Types.ObjectId, ref:"Plan" },
     status: { type:String, enum: ["active", "canceled", "expired"], default: "active" },
     stripeSubscriptionId: { type:String, default:null },

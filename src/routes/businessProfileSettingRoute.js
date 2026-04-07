305b63e16accd5ad87e9c4e658a9d620aeda3c1b
Techforge25 <github@techforgeinnovations.com>
2026-04-07 13:26:22 +0500

    Update company identity router

--- src/routes/businessProfileSettingRoute.js
+++ src/routes/businessProfileSettingRoute.js
@@ -1,6 +1,13 @@
 const { Router } = require("express");
+const { authentication, authorization } = require("../middlewares/auth");
+const { checkSubscription } = require("../middlewares/checkSubscription");
+const { updateCompanyIdentity } = require("../controllers/businessProfileSettingController");
 
 // Route instance
 const businessProfileSettingRouter = Router();
+
+// Update company identity
+businessProfileSettingRouter.route("/companyIdentity")
+.patch(authentication, authorization(["business"]), checkSubscription, updateCompanyIdentity);
 
 module.exports = businessProfileSettingRouter;

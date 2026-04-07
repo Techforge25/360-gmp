54ccd41cae68960576490192442aeeea2992f6e3
Techforge25 <github@techforgeinnovations.com>
2026-04-07 13:30:05 +0500

    Update validator

--- src/validations/businessProfileSettingsValidator.js
+++ src/validations/businessProfileSettingsValidator.js
@@ -1,23 +1,23 @@
 const joi = require("joi");
 
 // Patterns
-const alphaNumericPattern = /^[a-zA-Z0-9 -]*$/;
+const alphaNumericPattern = /^[a-zA-Z0-9\s\-.'&+()]*$/;
 const addressPattern = /^[a-zA-Z0-9 -,]*$/;
 const customPattern = /^[a-zA-Z0-9 \-.,\n\r]*$/;
 const stackHoldersNamePattern = /^[a-zA-Z]*$/;
 
 // Update company identity validator
 const updateCompanyIdentityValidator = joi.object({
-    ownerName: joi.string().min(3).max(200).trim().pattern(alphaNumericPattern).label("Owner name"),
-    identificationOfBusinessOwner: joi.string().min(3).max(200).trim().pattern(alphaNumericPattern).label("Identification of business owner"),
-    companyName: joi.string().min(5).max(200).trim().pattern(alphaNumericPattern).label("Company name"),
-    tradeName: joi.string().max(200).trim().optional().pattern(alphaNumericPattern).label("Trade name"),
-    businessType: joi.string().max(50).trim().allow("", null).pattern(alphaNumericPattern).label("Business type"),
-    companySize: joi.string().trim().allow("", null).pattern(alphaNumericPattern).label("Company size"),
-    foundedDate: joi.date().allow(null),
+    ownerName: joi.string().min(3).max(200).trim().required().pattern(alphaNumericPattern).label("Owner name"),
+    identificationOfBusinessOwner: joi.string().min(3).max(200).trim().required().pattern(alphaNumericPattern).label("Identification of business owner"),
+    companyName: joi.string().min(5).max(200).trim().required().pattern(alphaNumericPattern).label("Company name"),
+    tradeName: joi.string().max(200).trim().required().pattern(alphaNumericPattern).label("Trade name"),
+    businessType: joi.string().max(50).trim().required().pattern(alphaNumericPattern).label("Business type"),
+    companySize: joi.string().trim().required().pattern(alphaNumericPattern).label("Company size"),
+    foundedDate: joi.date().max('now').allow(null),
     primaryIndustry: joi.string().trim().allow("", null).pattern(alphaNumericPattern).label("Primary industry"),
     operationHour: joi.string().trim().allow("", null).label("Operation hours"),
-    countryOfRegistration: joi.string().max(50).trim().allow("", null).pattern(alphaNumericPattern).label("Country of registration"),
+    countryOfRegistration: joi.string().max(50).trim().required().pattern(alphaNumericPattern).label("Country of registration"),
 
     // Web and description
     website: joi.string().uri().max(100).trim().allow("", null).label("Website"),

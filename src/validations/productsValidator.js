60f4d98e2e32c64cc942e5854a2828ad0b3dc37e
Techforge25 <github@techforgeinnovations.com>
2026-04-08 16:10:55 +0500

    Increase production description length

--- src/validations/productsValidator.js
+++ src/validations/productsValidator.js
@@ -12,7 +12,7 @@ const createProductSchema = Joi.object({
     }),
     image: Joi.string().uri().required().label("Main image"),
     groupImages: Joi.array().items(Joi.string().uri()).max(3).label("Group images"),
-    detail: Joi.string().trim().max(2000).allow("", null).optional().label("Product description"),
+    detail: Joi.string().trim().max(5000).allow("", null).optional().label("Product description"),
     category: Joi.string().required().label("Product category"),
     pricePerUnit: Joi.number().positive().required().label("Price per unit"),
 

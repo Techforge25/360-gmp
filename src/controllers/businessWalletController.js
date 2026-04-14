d912c5242d7d0868c92bfc495a08360531748ede
Techforge25 <github@techforgeinnovations.com>
2026-04-14 11:43:17 +0500

    Add type of transfer

--- src/controllers/businessWalletController.js
+++ src/controllers/businessWalletController.js
@@ -8,6 +8,9 @@ const Withdrawal = require("../models/withdrawalModel");
 const Transaction = require("../models/transactionModel");
 const convertToMongoId = require("../utils/convertToMongoId");
 const { emptyList } = require("../constants");
+const validate = require("../utils/validate");
+const { transferFundsValidator } = require("../validations/walletValidator");
+const { startSession } = require("mongoose");
 
 // Fetch wallet analytics for business
 const fetchBusinessWalletAnalytics = asyncHandler(async (request, response) => {
@@ -272,5 +275,48 @@ const fetchBusinessWithdrawal = asyncHandler(async (request, response) => {
     return response.status(200).json(new ApiResponse(200, withdrawal, "Business withdrawals have been fetched"));    
 });
 
+// Transfer funds between business and user
+const transferFunds = asyncHandler(async (request, response) => {
+    // Get user ID and sub profile IDs
+    const userId = request.user._id;
+    const { userProfileId, businessProfileId } = request.user.profiles || {};
+
+    // Validate IDs
+    if(!businessProfileId) throw new ApiError(400, "Business profile ID is missing. You need to create a business profile before transferring funds");
+    if(!userProfileId) throw new ApiError(400, "User profile ID is missing. You need to create a user profile before transferring funds");    
+
+    // Get validated payload
+    const { amount, recipientType } = validate(transferFundsValidator, request.body);
+
+    // If user
+    if(recipientType === "user") 
+    {
+        // Check if business wallet exist and has sufficient balance
+        const businessWallet = await Wallet.findOne({ ownerId:businessProfileId, ownerModel:"BusinessProfile" });
+        if(!businessWallet) throw new ApiError(404, "Business wallet not found");
+        if(Number(businessWallet.availableBalance) < amount) throw new ApiError(400, "Insufficient balance in business wallet");
+
+        // Check if user wallet exist        
+        const userWallet = await Wallet.findOne({ ownerId:userProfileId, ownerModel:"UserProfile" });
+        if(!userWallet) throw new ApiError(404, "User wallet not found");
+
+        // Create db session
+        const dbSession = await startSession();
+        dbSession.startTransaction();
+
+        try
+        {
+            await Wallet.findOneAndUpdate(
+                {  }
+            )
+        }
+        catch(error)
+        {
+            await dbSession.abortTransaction();
+            dbSession.endSession()
+        }
+    }
+});
+
 module.exports = { fetchBusinessWalletAnalytics, fetchBusinessRecentTransactions, 
-fetchBusinessFinancialPerformance, fetchBusinessEarnings, fetchBusinessWithdrawal };
+fetchBusinessFinancialPerformance, fetchBusinessEarnings, fetchBusinessWithdrawal, transferFunds };

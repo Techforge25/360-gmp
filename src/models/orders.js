2ceab00c2db196ae36f202acdc07a2a4e7df8405
Techforge25 <github@techforgeinnovations.com>
2026-04-17 17:09:36 +0500

    Add status mode of dispute to order schema

--- src/models/orders.js
+++ src/models/orders.js
@@ -9,7 +9,7 @@ const orderSchema = new Schema({
     totalAmount: { type: Number },
     status: { 
         type: String,
-        enum: ["pending", "paid", "processing", "in-transit", "shipped", "delivered", "completed", "cancelled"],
+        enum: ["pending", "paid", "processing", "in-transit", "shipped", "delivered", "completed", "cancelled", "dispute"],
         default: "pending" 
     },
     items: [{

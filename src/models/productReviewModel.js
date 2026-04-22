7cea43b5b3fccde5b132fcb48c7ddf8edeb5eb26
Techforge25 <github@techforgeinnovations.com>
2026-04-22 17:44:23 +0500

    Add pagination plugin

--- src/models/productReviewModel.js
+++ src/models/productReviewModel.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const aggregatePaginate = require("mongoose-aggregate-paginate-v2");
 
 // Schema
 const productReviewSchema = new Schema({
@@ -11,6 +12,9 @@ const productReviewSchema = new Schema({
     comment: { type:String, trim:true, required:true },
     images: [{ type:String, trim:true }]
 }, { timestamps:true });
+
+// Inject plugin
+productReviewSchema.plugin(aggregatePaginate);
 
 // Model
 const ProductReview = model("ProductReview", productReviewSchema);

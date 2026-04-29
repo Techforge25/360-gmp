9fefd50ee72f84ee412d2d5a1b63531fb5339e95
Techforge25 <github@techforgeinnovations.com>
2026-04-29 11:18:50 +0500

    Refactor notification model

--- src/utils/sendNotification.js
+++ src/utils/sendNotification.js
@@ -2,32 +2,18 @@ const Notification = require("../models/notificationsModel");
 const ApiError = require("./ApiError");
 
 // Send notification helper
-const sendNotification = async ({ userId = null, title, content, type, io }) => {
+const sendNotification = async ({ userId, title, content, type, io }) => {
    try 
    {
       // Validate type
-      if(!["System", "UserProfile", "BusinessProfile", "Public"].includes(type)) throw new ApiError(400, "Invalid notification type");
-   
-      // Enforce user owner id for non-public notification
-      if(type !== "Public" && !userId) throw new ApiError(400, "User ID is required for non-public notifications");  
+      if(!["System", "UserProfile", "BusinessProfile"].includes(type)) throw new ApiError(400, "Invalid notification type");
 
       // Save to db
       const notification = await Notification.create({ userId, title, content, type });
       if(!notification) throw new ApiError(400, "Failed to create notification! Invalid payload");
 
-      // Notification payload for socket
-      const notificationPayload = { title, content, type, createdAt: notification.createdAt };
-
-      if(type === "Public")
-      {
-         // Public notification
-         io.emit("notification", notificationPayload);
-      }
-      else
-      {
-         // Private notification
-         io.to(String(userId)).emit("notification", notificationPayload);
-      }      
+      // Emit real-time notification
+      io.to(String(userId)).emit("notification", { title, content, type, createdAt: notification.createdAt });    
    } 
    catch(error) 
    {

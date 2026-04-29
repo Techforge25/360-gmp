3125838fcc14d866db4c5847555f90bb79ab344e
Techforge25 <github@techforgeinnovations.com>
2026-04-29 11:19:04 +0500

    Refactor notification model

--- src/models/notificationsModel.js
+++ src/models/notificationsModel.js
@@ -3,7 +3,7 @@ const paginate = require("mongoose-paginate-v2");
 
 // Schema
 const notificationSchema = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: "User", default:null },
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     title: { type: String, trim: true, required: true },
     content: { type: String, trim: true, required: true },
     type: { type: String, enum: ["System", "UserProfile", "BusinessProfile"], default:"System" },

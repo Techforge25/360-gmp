07f8eed8d5313b7dc4f7062bf4489d94565f7554
Techforge25 <github@techforgeinnovations.com>
2026-05-01 10:54:39 +0500

    Set status

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -617,7 +617,7 @@ const updateOrderStatusBySeller = asyncHandler(async (request, response) => {
 
     // Emit event real-time
     const io = request.app.get("io");
-    io.to(String(order.buyerUserProfileId._id)).emit("update-order-status", { orderId:order._id, status:order.status });
+    io.to(String(order.buyerUserProfileId._id)).emit("update-order-status", { orderId:order._id, status });
 
     // Send notification to user profile
     await sendNotification({

9a7946a57c09062050b88d191db1f5c551376b41
Techforge25 <github@techforgeinnovations.com>
2026-06-03 10:08:19 +0500

    Update email label

--- src/validations/verifyPasswordResetTokenValidator.js
+++ src/validations/verifyPasswordResetTokenValidator.js
@@ -1,7 +1,7 @@
 const joi = require("joi");
 
 const verifyPasswordResetTokenSchema = joi.object({
-    email: joi.string().trim().lowercase().email().required().label("Email address"),
+    email: joi.string().trim().lowercase().email().required().label("Email"),
     passwordResetToken: joi.string().trim().required().label("Password reset token")
 });
 

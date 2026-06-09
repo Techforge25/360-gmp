a1fa7ea9392ae9527da4f5ff7c527756395237b6
Techforge25 <github@techforgeinnovations.com>
2026-06-09 09:58:45 +0500

    Test

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 // Cors options
 const corsOptions = {
-    origin:[process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin:[process.env.FRONTEND_URL, "http://localhost:3000", "http://localhost:5173"],
     credentials:true,
     methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders:["Content-Type", "Authorization"]
@@ -22,11 +22,11 @@ const corsOptions = {
 
 // Cookie options
 const cookieOptions = {
-    httpOnly:true,
+    httpOnly: true,
     secure: process.env.NODE_ENV === "local" ? false : true,
     signed: true,
     maxAge: 1000 * 60 * 60 * 24 * 90,
-    sameSite: process.env.NODE_ENV === "local" ? "lax" : "none",
+    sameSite: process.env.NODE_ENV === "local" ? "none" : "none",
     domain: process.env.NODE_ENV === "local" ? undefined : ".techforgeinnovations.com"
 };
 

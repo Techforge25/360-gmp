ec3aad07e82df0571c37b763c5ba84816cf77c3e
Techforge25 <github@techforgeinnovations.com>
2026-06-10 15:43:09 +0500

    Setup multi environments

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-require("dotenv").config({ path:`.env.${process.env.NODE_ENV}` });
+// require("dotenv").config({ path:`.env.${process.env.NODE_ENV}` });
+require("dotenv").config();
 const http = require("http"); 
 const { Server } = require("socket.io"); 
 const app = require("./app");

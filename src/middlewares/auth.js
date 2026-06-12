5df25ca742f1d9d295f3c9e12236808a5cf4952d
Techforge25 <github@techforgeinnovations.com>
2026-06-12 13:21:04 +0500

    Changes in middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,17 +1,21 @@
 const { getAccessToken, verifyAccessToken } = require("../utils/accessToken");
 const ApiError = require("../utils/ApiError");
+const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
+
+// Redirect url
+const redirectURL = `${process.env.FRONTEND_URL}/login`;
 
 // Authentication
 const authentication = asyncHandler((request, response, next) => {
     const accessToken = getAccessToken(request);
-    // if(!accessToken) throw new ApiError(401, "Unauthorized!");
-    if(!accessToken) return response.status(303).redirect(`${process.env.FRONTEND_URL}/login`);
+    if(!accessToken) throw new ApiError(401, "Unauthorized!");
+    //if(!accessToken) return response.status(401).json(new ApiResponse(401, { redirectURL }, "Unauthorized!"));
 
     // Verify
     const user = verifyAccessToken(accessToken);
-    // if(!user) throw new ApiError(401, "Invalid access token");
-    if(!user) return response.status(303).redirect(`${process.env.FRONTEND_URL}/login`);
+    if(!user) throw new ApiError(401, "Invalid access token");
+    // if(!user) return response.status(401).json(new ApiResponse(401, { redirectURL }, "Unauthorized"));
 
     // Pass through
     request.user = user;

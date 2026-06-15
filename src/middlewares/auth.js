4680bea43ef840ef5718543b9254a8d5b4d6cf53
Techforge25 <github@techforgeinnovations.com>
2026-06-15 13:02:40 +0500

    Server redirection

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,13 +9,13 @@ const redirectURL = `${process.env.FRONTEND_URL}/login`;
 // Authentication
 const authentication = asyncHandler((request, response, next) => {
     const accessToken = getAccessToken(request);
-    if(!accessToken) throw new ApiError(401, "Unauthorized!");
-    //if(!accessToken) return response.status(401).json(new ApiResponse(401, { redirectURL }, "Unauthorized!"));
+    // if(!accessToken) throw new ApiError(401, "Unauthorized!");
+    if(!accessToken) return response.status(401).json(new ApiResponse(401, { redirectURL }, "Unauthorized!"));
 
     // Verify
     const user = verifyAccessToken(accessToken);
-    if(!user) throw new ApiError(401, "Invalid access token");
-    // if(!user) return response.status(401).json(new ApiResponse(401, { redirectURL }, "Unauthorized"));
+    // if(!user) throw new ApiError(401, "Invalid access token");
+    if(!user) return response.status(401).json(new ApiResponse(401, { redirectURL }, "Unauthorized"));
 
     // Pass through
     request.user = user;

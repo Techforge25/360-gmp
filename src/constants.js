cdd061b89c47bf879010a91cfaa6574f1ecf7a8d
Techforge25 <github@techforgeinnovations.com>
2026-07-01 10:39:17 +0500

    Set samesite to none

--- src/constants.js
+++ src/constants.js
@@ -25,7 +25,8 @@ const cookieOptions = {
     secure: isLocal ? false : true,
     signed: true,
     maxAge: 1000 * 60 * 60 * 24 * 90,
-    sameSite: isLocal ? "lax" : "none",
+    sameSite: "none",
+    // sameSite: isLocal ? "lax" : "none",
     // domain: isProduction ? ".techforgeinnovations.com" : undefined
 };
 

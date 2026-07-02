6a116a7b07c03296a8a1a230ebec5f544e4d8b10
Techforge25 <github@techforgeinnovations.com>
2026-07-02 15:04:00 +0500

    Clear cookie

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -261,18 +261,46 @@ const refreshAccessToken = asyncHandler(async (request, response) => {
 
     // Get token
     const token = getRefreshToken(request);
-    if(!token) throw new ApiError(401, "Unauthorized! Refresh token is missing");
+    // if(!token) throw new ApiError(401, "Unauthorized! Refresh token is missing");
+    if(!token)
+    {
+        return response.status(401)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(new ApiResponse(401, { redirectURL }, "Unauthorized! Refresh token is missing"));
+    }
 
     // Verify refresh token
     const payload = verifyRefreshToken(token);
-    if(!payload) throw new ApiError(401, "Unauthorized! Invalid refresh token");
+    // if(!payload) throw new ApiError(401, "Unauthorized! Invalid refresh token");
+    if(!payload) 
+    {
+        return response.status(401)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(new ApiResponse(401, { redirectURL }, "Unauthorized! Invalid refresh token")); 
+    }
 
     // Find user
     const user = await User.findById(payload._id).select("_id role refreshToken");
-    if(!user) throw new ApiError(401, "User not found associated with the provided refresh token");
+    // if(!user) throw new ApiError(401, "User not found associated with the provided refresh token");
+    if(!user) 
+    {
+        return response.status(401)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(new ApiResponse(401, { redirectURL }, "User not found associated with the provided refresh token"));         
+    }
 
     // Compare tokens
-    if(user.refreshToken !== token) throw new ApiError(401, "Refresh token mismatch");
+    // if(user.refreshToken !== token) throw new ApiError(401, "Refresh token mismatch");
+    if(user.refreshToken !== token)
+    {
+        return response.status(401)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
+        .json(new ApiResponse(401, { redirectURL }, "Refresh token mismatch"));         
+    }
 
     // Fetch child profiles
     const [userProfile, businessProfile] = await Promise.all([

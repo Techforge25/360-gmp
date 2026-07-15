1fae1db44294df39bc76a2c29db04f931edac17f
Techforge25 <github@techforgeinnovations.com>
2026-07-15 12:03:19 +0500

    Modify: Admin schema

--- src/models/adminModel.js
+++ src/models/adminModel.js
@@ -5,8 +5,9 @@ const bcrypt = require("bcrypt");
 const adminSchema = new Schema({
     username: { type:String, trim:true, lowercase:true, required:true, unique:[true, "This username has already been taken by another admin"] },
     password: { type:String, trim:true, required:true },
-    role: { type:String, enum:["admin"], default:"admin" },
-    refreshToken: { type:String, default:null }
+    role: { type:String, enum:["superAdmin", "admin"], default: "admin" },
+    allowedModules: { type:[String], default: [] },
+    refreshToken: { type: String, default: null }
 });
 
 // Hash password

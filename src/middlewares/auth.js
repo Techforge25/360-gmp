c3e68ca8ca1c6bddfb7c93f94f6ad56769dc170d
Techforge25 <github@techforgeinnovations.com>
2026-07-20 11:17:26 +0500

    Update: Exclude localhost URI

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,8 +5,7 @@ const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
 
 // Redirect url
-// const redirectURL = `${frontendURL}`;
-const redirectURL = `http://localhost:3000`;
+const redirectURL = `${frontendURL}`;
 
 // Authentication
 const authentication = asyncHandler((request, response, next) => {

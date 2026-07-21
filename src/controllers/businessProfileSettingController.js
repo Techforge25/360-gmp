13d8f04ee87c7aeba460e0ccfc621e22840b4031
Techforge25 <github@techforgeinnovations.com>
2026-07-21 10:12:24 +0500

    Create API to update business info

--- src/controllers/businessProfileSettingController.js
+++ src/controllers/businessProfileSettingController.js
@@ -4,117 +4,46 @@ const ApiError = require("../utils/ApiError");
 const ApiResponse = require("../utils/ApiResponse");
 const asyncHandler = require("../utils/asyncHandler");
 const validate = require("../utils/validate");
-const { updateCompanyIdentityValidator, operationsAndLogisticsValidator, updateBusinessIntelligenceValidator, 
-updateBusinessCertifactesValidator } = require("../validations/businessProfileSettingsValidator");
+const { updateCompanyIdentityValidator, updateBusinessInfoValidator } = require("../validations/businessProfileSettingsValidator");
 
-// Update company identity
-const updateCompanyIdentity = asyncHandler(async (request, response) => {
+// Fetch business settings info
+const fetchBusinessInfo = asyncHandler(async (request, response) => {
     const { businessProfileId } = request.user.profiles || {};
 
-    // Sanitize
-    if(!businessProfileId) throw new ApiError(400, "Business profile not found");
-    if(!isValidObjectId(businessProfileId)) throw new ApiError(400, "Invalid business profile ID");
-   
+    // Fetch
+    const businessProfile = await BusinessProfile.findById(businessProfileId)
+    .select(`tradeName companySize operationHour website warehouseAddress additionalWarehouseAddress 
+    incoterms termsAndCapability internationalOffices primaryContactPerson operationalAndTradeProfile description`);
+    if(!businessProfile) throw new ApiError(404, "Business profile not found");
+
+    // Response
+    return response.status(200).json(new ApiResponse(200, businessProfile, "Business profile info has been fetched"));
+});
+
+// Update business info
+const updateBusinessInfo = asyncHandler(async (request, response) => {
+    const { businessProfileId } = request.user.profiles || {};
+
     // Get validated payload
-    const { ownerName, identificationOfBusinessOwner, companyName, tradeName, businessType, 
-    companySize, foundedDate, primaryIndustry, operationHour, countryOfRegistration, website,
-    description, businessRegistrationNumber, taxIdentificationNumber, dunsNumber, 
-    complianceScreeningStatus } = validate(updateCompanyIdentityValidator, request.body) || {};
+    const { tradeName, companySize, operationHour, website, description, warehouseAddress, 
+    additionalWarehouseAddress, incoterms, termsAndCapability, internationalOffices, 
+    primaryContactPerson, operationalAndTradeProfile } = validate(updateBusinessInfoValidator, request.body);
 
     // Update
     const business = await BusinessProfile.findByIdAndUpdate(
         businessProfileId, 
         { 
             $set: { 
-                ownerName, identificationOfBusinessOwner, companyName, tradeName, businessType, 
-                companySize, foundedDate, primaryIndustry, operationHour, countryOfRegistration, website,
-                description, businessRegistrationNumber, taxIdentificationNumber, dunsNumber, 
-                complianceScreeningStatus
+                tradeName, companySize, operationHour, website, description, warehouseAddress, 
+                additionalWarehouseAddress, incoterms, termsAndCapability, internationalOffices, 
+                primaryContactPerson, operationalAndTradeProfile
             } 
-        }, 
-        { new:true, runValidators:true }
+        }
     );
-    if(!business) throw new ApiError(404, "Failed to update company identity");
+    if(!business) throw new ApiError(404, "Business profile not found");
 
     // Response
-    return response.status(200).json(new ApiResponse(200, null, "Company identity has been updated"));
+    return response.status(200).json(new ApiResponse(200, null, "Business info has been updated"));
 });
 
-// Update operations & logistics
-const updateOperationsAndLogistics = asyncHandler(async (request, response) => {
-    const { businessProfileId } = request.user.profiles || {};
-
-    // Sanitize
-    if(!businessProfileId) throw new ApiError(400, "Business profile not found");
-    if(!isValidObjectId(businessProfileId)) throw new ApiError(400, "Invalid business profile ID");
-   
-    // Get validated payload
-    const { location, incoterms, shipping, standardProductDimensions } = validate(operationsAndLogisticsValidator, request.body) || {};
-
-    // Update
-    const business = await BusinessProfile.findByIdAndUpdate(
-        businessProfileId, 
-        { $set: { location, incoterms, "shipping.capabilities": shipping.capabilities, standardProductDimensions } }, 
-        { new:true, runValidators:true }
-    );
-    if(!business) throw new ApiError(404, "Failed to update operations and logistics");
-
-    // Response
-    return response.status(200).json(new ApiResponse(200, null, "Company operations and logistics have been updated"));
-});
-
-// Update business intelligence
-const updateBusinessIntelligence = asyncHandler(async (request, response) => {
-    const { businessProfileId } = request.user.profiles || {};
-
-    // Sanitize
-    if(!businessProfileId) throw new ApiError(400, "Business profile not found");
-    if(!isValidObjectId(businessProfileId)) throw new ApiError(400, "Invalid business profile ID");
-   
-    // Get validated payload
-    const { b2bContact, executiveLeadership, stakeholderDisclosure, regionOfOperations,
-    productionCapacity, tradeAffiliations, auditingAgency } = validate(updateBusinessIntelligenceValidator, request.body) || {};
-
-    // Update
-    const business = await BusinessProfile.findByIdAndUpdate(
-        businessProfileId, 
-        { 
-            $set: {
-                b2bContact, executiveLeadership, stakeholderDisclosure, regionOfOperations,
-                productionCapacity, tradeAffiliations, auditingAgency
-            } 
-        }, 
-        { new:true, runValidators:true }
-    );
-    if(!business) throw new ApiError(404, "Failed to update business intelligence");
-
-    // Response
-    return response.status(200).json(new ApiResponse(200, null, "Business intelligence has been updated"));
-});
-
-// Update certificates
-const updateBusinessCertificates = asyncHandler(async (request, response) => {
-    const { businessProfileId } = request.user.profiles || {};
-
-    // Sanitize
-    if(!businessProfileId) throw new ApiError(400, "Business profile not found");
-    if(!isValidObjectId(businessProfileId)) throw new ApiError(400, "Invalid business profile ID");
-   
-    // Get validated payload
-    const { certificateOfIncorporation, taxRegistrationCertificate, 
-    certifications } = validate(updateBusinessCertifactesValidator, request.body) || {};
-
-    // Update
-    const business = await BusinessProfile.findByIdAndUpdate(
-        businessProfileId, 
-        { $set: { certificateOfIncorporation, taxRegistrationCertificate, certifications } }, 
-        { new:true, runValidators:true }
-    );
-    if(!business) throw new ApiError(404, "Failed to update business certificates");
-
-    // Response
-    return response.status(200).json(new ApiResponse(200, null, "Business certificates have been updated"));
-});
-
-module.exports = { updateCompanyIdentity, updateOperationsAndLogistics, 
-updateBusinessIntelligence, updateBusinessCertificates };
+module.exports = { fetchBusinessInfo, updateBusinessInfo };
--- src/routes/businessProfileSettingRoute.js
+++ src/routes/businessProfileSettingRoute.js
@@ -1,26 +1,14 @@
 const { Router } = require("express");
 const { authentication, authorization } = require("../middlewares/auth");
 const { checkSubscription } = require("../middlewares/checkSubscription");
-const { updateCompanyIdentity, updateOperationsAndLogistics, 
-updateBusinessIntelligence, updateBusinessCertificates } = require("../controllers/businessProfileSettingController");
+const { fetchBusinessInfo, updateBusinessInfo } = require("../controllers/businessProfileSettingController");
 
 // Route instance
 const businessProfileSettingRouter = Router();
 
 // Update company identity
-businessProfileSettingRouter.route("/companyIdentity")
-.patch(authentication, authorization(["business"]), checkSubscription, updateCompanyIdentity);
-
-// Update operations & logistics
-businessProfileSettingRouter.route("/operationsAndLogistics")
-.patch(authentication, authorization(["business"]), checkSubscription, updateOperationsAndLogistics);
-
-// Update business intelligence
-businessProfileSettingRouter.route("/businessIntelligence")
-.patch(authentication, authorization(["business"]), checkSubscription, updateBusinessIntelligence);
-
-// Update business intelligence
-businessProfileSettingRouter.route("/businessCertificates")
-.patch(authentication, authorization(["business"]), checkSubscription, updateBusinessCertificates);
+businessProfileSettingRouter.route("/info")
+.get(authentication, authorization(["business"]), checkSubscription, fetchBusinessInfo)
+.patch(authentication, authorization(["business"]), checkSubscription, updateBusinessInfo);
 
 module.exports = businessProfileSettingRouter;

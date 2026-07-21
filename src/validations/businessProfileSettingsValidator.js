a80655699cbc0ac3ec8a5728ac90541e1dd6f356
Techforge25 <github@techforgeinnovations.com>
2026-07-21 09:49:50 +0500

    Add constant

--- src/validations/businessProfileSettingsValidator.js
+++ src/validations/businessProfileSettingsValidator.js
@@ -1,45 +1,58 @@
 const joi = require("joi");
-
-// Patterns
-const alphaNumericPattern = /^[a-zA-Z0-9\s\-.'&]*$/;
-const ownerNamePattern = /^[a-zA-Z\s\-.']*$/;
-const countryPattern = /^[a-zA-Z\s-]*$/;
-const cityPattern = /^(?=.*[A-Za-zÀ-ÖØ-öø-ÿ])[A-Za-z0-9À-ÖØ-öø-ÿ\s\-'.]+$/;
-const addressPattern = /^(?![0-9\s,.\-/#]+$)[a-zA-Z0-9\s,.\-/#]+$/;
-const customPattern = /^[a-zA-Z0-9 \-.,\n\r]*$/;
-const operatingHourPattern = /^(0?[1-9]|1[0-2])(AM|PM|am|pm)\s-\s(0[1-9]|1[0-2])(AM|PM|am|pm)$/;
-const alphaNumericWithHyphen = /^[A-Za-z0-9-]+$/;
-const websitePattern = /^https?:\/\/.+$/;
-const dunsPattern = /^\d{9}$/;
-const contactNamePattern = /^[A-Za-z\s\-.']*$/;
-const titlePattern = /^[A-Za-z\s\-&]*$/;
-const phonePattern = /^[\d+\\s()]+$/;
-const tradeAffiliationPattern = /^[A-Za-z0-9 -]*$/;
-const auditingAgencyPattern = /^[A-Za-z0-9\s,\-&]*$/;
-const emailPattern = /^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,63}$/;
-const primaryIndustryPattern = /^[A-Za-z0-9\- &/]*$/;
-
-// Critical
-const identificationPattern = /^[A-Za-z0-9Ññ\- .\/&]+$/;
+const { allowedIncoterms, allowedTermsAndCapability } = require("../constants");
 
 // Company Identity
-const updateCompanyIdentityValidator = joi.object({
-    ownerName: joi.string().pattern(ownerNamePattern).min(2).max(100).trim().required().label("Owner name"),
-    identificationOfBusinessOwner: joi.string().pattern(identificationPattern).min(5).max(20).trim().required().label("Identification of business owner"),
-    companyName: joi.string().pattern(alphaNumericPattern).min(3).max(50).trim().required().label("Company name"),
-    tradeName: joi.string().pattern(alphaNumericPattern).min(2).max(150).trim().optional().allow("", null).label("Trade name"),
-    businessType: joi.string().trim().required().invalid("select").label("Business type"),
-    companySize: joi.string().trim().required().invalid("select").label("Company size"),
-    foundedDate: joi.date().max('now').min('1800-01-01').allow(null),
-    primaryIndustry: joi.string().pattern(primaryIndustryPattern).min(2).max(100).trim().allow("", null).label("Primary industry"),
-    operationHour: joi.string().max(100).trim().allow("", null).pattern(operatingHourPattern).label("Operation hours"),
-    countryOfRegistration: joi.string().trim().required().invalid("select").label("Country of registration"),
-    website: joi.string().pattern(websitePattern).min(8).max(225).trim().allow("", null).label("Website"),
-    description: joi.string().trim().min(10).max(5000).allow("", null).label("Business description"),
-    businessRegistrationNumber: joi.string().pattern(identificationPattern).min(5).max(20).trim().required().label("Business registration number"),
-    taxIdentificationNumber: joi.string().pattern(identificationPattern).min(5).max(20).trim().required().label("Tax identification number"),
-    dunsNumber: joi.string().pattern(dunsPattern).length(9).trim().allow("", null).label("Data Universal Numbering System"),
-    complianceScreeningStatus: joi.boolean().default(true).label("Compliance screening status"),
+const updateBusinessInfoValidator = joi.object({
+    tradeName: joi.string().max(200).trim().optional().allow("", null).label("Trade name"),
+    companySize: joi.string().trim().required().label("Company size"),
+    operationHour: joi.string().max(50).trim().required().label("Operation hours"),
+    website: joi.string().uri().max(100).trim().allow("", null).label("Website"),
+    description: joi.string().trim().max(5000).required().label("Business description"),
+    
+    // Warehouse address
+    warehouseAddress: joi.object({
+        country: joi.string().max(100).trim().optional().allow(null, "").label("Warehouse country"),
+        city: joi.string().max(100).trim().optional().allow(null, "").label("Warehouse city"),
+        addressLine: joi.string().min(10).max(500).trim().optional().allow(null, "").label("Warehouse address line"),
+    }).optional().label("Warehouse Address"),   
+    
+    // Additional warehouse address
+    additionalWarehouseAddress: joi.array().items(joi.object({
+        country: joi.string().trim().max(100).allow("", null).label("Additional warehouse country"),
+        city: joi.string().trim().max(100).allow("", null).label("Additional warehouse city"),
+        addressLine: joi.string().trim().min(10).max(500).allow("", null).label("Additional warehouse address line"),        
+    })).optional().label("Additional Warehouse Address"),    
+
+    // International Commercial Terms
+    incoterms: joi.string().max(500).trim().valid(...allowedIncoterms).label("International Commercial Terms"),
+    termsAndCapability: joi.string().max(500).valid(...allowedTermsAndCapability).label("Terms and capability"), 
+    
+    /* INTERNATIONAL OFFICES */
+    internationalOffices: joi.array().max(5).items(joi.object({
+        officeName: joi.string().trim().min(2).max(50).optional().allow("", null).label("Office name"),
+        country: joi.string().trim().max(100).optional().allow("", null).label("Country"),
+        city: joi.string().trim().max(100).optional().allow("", null).label("City"),
+        state: joi.string().trim().max(100).optional().allow("", null).label("State"),
+        addressLine: joi.string().trim().min(10).optional().max(500).allow("", null).label("Address line"), 
+        zipCode: joi.string().trim().min(5).max(50).optional().allow("", null).label("Zip code"), 
+    })).optional().label("International Offices"),
+    
+    /* BUSINESS INTELLIGENCE & LEADERSHIP */
+    primaryContactPerson: joi.object({
+        name: joi.string().trim().min(2).max(50).required().label("Name"),
+        title: joi.string().trim().min(2).max(50).required().label("Title"),
+        phone: joi.string().trim().max(15).pattern(/^\+?[1-9]\d{9,14}$/).required().messages({
+            "string.pattern.base": "Phone number must be a valid international format (e.g., +923001234567)."
+        }).required().label("Phone"),
+        supportEmail: joi.string().trim().email().lowercase().required().label("Support Email")
+    }).required().label("Primary Contact Person"),
+    
+    /* OPERATIONAL & TRADE PROFILE */
+    operationalAndTradeProfile: joi.object({
+        auditingAgency: joi.string().trim().optional().label("Auditing agency"),
+        regionOfOperations: joi.array().min(1).items(joi.string().trim()).label("Region of operations"),  
+        tradeAffiliations: joi.array().max(5).items(joi.string().optional().allow(null, "")).label("Trade Affiliations"),
+    }).optional().label("Operational and trade profile")    
 });
 
-module.exports = { updateCompanyIdentityValidator };
+module.exports = { updateBusinessInfoValidator };

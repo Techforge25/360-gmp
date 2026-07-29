e1af18e71c363d27d2658b5133470d208599d806
Techforge25 <github@techforgeinnovations.com>
2026-07-29 11:20:31 +0600

    Create update admin validator

--- src/validations/adminValidator.js
+++ src/validations/adminValidator.js
@@ -2,6 +2,19 @@ const joi = require("joi");
 
 // Create admin validator
 const createAdminValidator = joi.object({
+    username: joi.string().trim().lowercase().min(3).max(20).required().label("Username"),
+    email: joi.string().trim().lowercase().email().required().label("Email"),
+    password: joi.string().trim().min(3).max(50).required()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&^#()[\\]{}|\\\\<>+=._-])[A-Za-z\\d@$!%*?&^#()[\\]{}|\\\\<>+=._-]+$"))
+    .required().messages({
+        "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+        "string.min": "Password must be at least 8 characters long."
+    }).label("Password"),
+    allowedModules: joi.array().max(5).items(joi.string().trim()).default([]).label("Allowed modules")
+});
+
+// Update admin validator
+const updateAdminValidator = joi.object({
     username: joi.string().trim().min(3).max(20).required().label("Username"),
     password: joi.string().trim().min(3).max(50).required()
     .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&^#()[\\]{}|\\\\<>+=._-])[A-Za-z\\d@$!%*?&^#()[\\]{}|\\\\<>+=._-]+$"))
@@ -12,9 +25,4 @@ const createAdminValidator = joi.object({
     allowedModules: joi.array().max(5).items(joi.string().trim()).default([]).label("Allowed modules")
 });
 
-// Assign module to admin validator
-const assignModuleValidator = joi.object({
-    allowedModules: joi.array().max(5).items(joi.string().trim()).default([]).label("Allowed modules")
-});
-
-module.exports = { createAdminValidator, assignModuleValidator };
+module.exports = { createAdminValidator, updateAdminValidator };

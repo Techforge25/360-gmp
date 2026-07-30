fc65f235178d3098700c66272e761134c588efee
Techforge25 <github@techforgeinnovations.com>
2026-07-30 13:30:30 +0600

    Create API to fetch marketplace stats

--- src/controllers/admin/marketplaceController.js
+++ src/controllers/admin/marketplaceController.js
@@ -5,59 +5,54 @@ const Order = require("../../models/orders");
 const Product = require("../../models/products");
 const ApiResponse = require("../../utils/ApiResponse");
 const asyncHandler = require("../../utils/asyncHandler");
+const getDateFilter = require("../../utils/dateFilter");
 
-// Sum total sales
-const sumTotalSales = asyncHandler(async (request ,response) => {
-    //Fetch
-    const totalSales = await EscrowTransaction.aggregate([
-        {
-            $group:{
-                _id:null,
-                totalAmount: { $sum: "$totalAmount" }
-            }
-        }
-    ]);
+// Fetch market place stats
+const fetchMarketplaceStats = asyncHandler(async (request, response) => {
+    // Get date filter
+    const { dateFilter } = getDateFilter(request);
 
-    // Prepare payload
-    const payload = { totalSales: Number(totalSales[0]?.totalAmount) || 0 };
-
-    // Response
-    return response.status(200).json(new ApiResponse(200, payload, "Sum of total sales have been fetched"));
-});
-
-// Sum pending products
-const sumPendingProducts = asyncHandler(async (request, response) => {
-    const pendingProducts = await Product.aggregate([
-        { $match:{ status:"pending" } },
-
-        // Sum
-        {
-            $group: {
-                _id:null,
-                totalPendingProducts:{ $sum:1 }
-            }
-        }
-    ]);
-
-    // Prepare payload
-    const payload = { totalPendingProducts: Number(pendingProducts[0]?.totalPendingProducts) || 0 };
-
-    // Response
-    return response.status(200).json(new ApiResponse(200, payload, "Sum of total pending products have been fetched"));    
-});
-
-// Sum disputed orders
-const sumDisputedOrders = asyncHandler(async (request, response) => {
     // Fetch
-    const disputedOrdersCount = await Dispute.countDocuments({ 
-        status: { $in:["open", "under_review", "waiting_seller"] } 
-    });
+    const [[totalSales], totalPendingProducts, totalDisputedOrders, [totalFundsHeldInEscrow]] = await Promise.all([
+        // Total sales
+        EscrowTransaction.aggregate([
+            { $match: { ...dateFilter, status: "released" } },
+            {
+                $group: {
+                    _id: null,
+                    totalAmount: { $sum: "$totalAmount" }
+                }
+            }
+        ]),
+        
+        // Pending products
+        Product.countDocuments({ ...dateFilter, status: "pending" }),
 
-    // Prepare payload
-    const payload = { totalDisputedOrders: Number(disputedOrdersCount) || 0 };
+        // Total dispute
+        Order.countDocuments({ ...dateFilter, status: "disputed" }),
+
+        // Total funds held in escrow
+        EscrowTransaction.aggregate([
+            { $match: { ...dateFilter, status: "held" } },
+            {
+                $group: {
+                    _id: null,
+                    totalAmount: { $sum: "$totalAmount" }
+                }
+            }            
+        ])
+    ]);
+
+    // Payload
+    const payload = {
+        totalSales: Number(totalSales?.totalAmount.toFixed(2)) || 0,
+        totalPendingProducts: Number(totalPendingProducts.toFixed(2)) || 0,
+        totalDisputedOrders: Number(totalDisputedOrders.toFixed(2)) || 0,
+        totalFundsHeldInEscrow: Number(totalFundsHeldInEscrow.totalAmount.toFixed(2)) || 0
+    };
 
     // Response
-    return response.status(200).json(new ApiResponse(200, payload, "Sum of disputed orders have been fetched"));   
+    return response.status(200).json(new ApiResponse(200, payload, "Marketplace stats have been fetched"));
 });
 
 // Fetch order logs
@@ -257,5 +252,4 @@ const fetchDisputedOrders = asyncHandler(async (request, response) => {
     return response.status(200).json(new ApiResponse(200, orders, "Disputed order logs have been fetched"));    
 });
 
-module.exports = { sumTotalSales, sumPendingProducts, sumDisputedOrders, 
-fetchOrderLogs, fetchProductAudits, fetchDisputedOrders };
+module.exports = { fetchMarketplaceStats, fetchOrderLogs, fetchProductAudits, fetchDisputedOrders };
--- src/routes/admin/marketplaceRoute.js
+++ src/routes/admin/marketplaceRoute.js
@@ -1,22 +1,14 @@
 const { Router } = require("express");
 const { adminAuthentication } = require("../../middlewares/adminAuth");
-const { sumTotalSales, sumPendingProducts, sumDisputedOrders, 
-fetchOrderLogs, fetchProductAudits, fetchDisputedOrders } = require("../../controllers/admin/marketplaceController");
+const { fetchMarketplaceStats, fetchOrderLogs, fetchProductAudits, 
+fetchDisputedOrders } = require("../../controllers/admin/marketplaceController");
 
 // Router instance
 const marketPlaceRouter = Router();
 
-// Sum total sales
-marketPlaceRouter.route("/totalSales")
-.get(adminAuthentication, sumTotalSales);
-
-// Sum pending products
-marketPlaceRouter.route("/pendingProducts")
-.get(adminAuthentication, sumPendingProducts);
-
-// Sum disputed orders
-marketPlaceRouter.route("/disputedOrders")
-.get(adminAuthentication, sumDisputedOrders);
+// Fetch marketplace stats
+marketPlaceRouter.route("/stats")
+.get(adminAuthentication, fetchMarketplaceStats);
 
 // Fetch order logs
 marketPlaceRouter.route("/orderLogs")

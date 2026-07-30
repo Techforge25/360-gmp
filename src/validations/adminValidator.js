5fa137f07af947e9cb94cca641086eb13a3887c3
Techforge25 <github@techforgeinnovations.com>
2026-07-30 18:45:48 +0600

    Exclude password from validators

--- src/validations/adminValidator.js
+++ src/validations/adminValidator.js
@@ -16,12 +16,6 @@ const createAdminValidator = joi.object({
 // Update admin validator
 const updateAdminValidator = joi.object({
     username: joi.string().trim().min(3).max(20).required().label("Username"),
-    password: joi.string().trim().min(3).max(50).required()
-    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&^#()[\\]{}|\\\\<>+=._-])[A-Za-z\\d@$!%*?&^#()[\\]{}|\\\\<>+=._-]+$"))
-    .required().messages({
-        "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
-        "string.min": "Password must be at least 8 characters long."
-    }).label("Password"),
     allowedModules: joi.array().max(5).items(joi.string().trim()).default([]).label("Allowed modules")
 });
 

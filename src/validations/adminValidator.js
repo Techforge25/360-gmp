f22553e74339a0a867802e088634f79d39bd522e
Techforge25 <github@techforgeinnovations.com>
2026-07-30 18:50:42 +0600

    Create validator for admin password

--- src/validations/adminValidator.js
+++ src/validations/adminValidator.js
@@ -19,4 +19,14 @@ const updateAdminValidator = joi.object({
     allowedModules: joi.array().max(5).items(joi.string().trim()).default([]).label("Allowed modules")
 });
 
-module.exports = { createAdminValidator, updateAdminValidator };
+// Update admin password validator
+const updateAdminPasswordValidator = joi.object({
+    password: joi.string().trim().min(3).max(50).required()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&^#()[\\]{}|\\\\<>+=._-])[A-Za-z\\d@$!%*?&^#()[\\]{}|\\\\<>+=._-]+$"))
+    .required().messages({
+        "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+        "string.min": "Password must be at least 8 characters long."
+    }).label("Password")
+});
+
+module.exports = { createAdminValidator, updateAdminValidator, updateAdminPasswordValidator };

a73d2d83125917dc37a3f003808db35540caeb83
Techforge25 <github@techforgeinnovations.com>
2026-08-06 13:42:53 +0600

    Make strpe price id as optional

--- src/models/plan.js
+++ src/models/plan.js
@@ -2,14 +2,14 @@ const { Schema, model } = require("mongoose");
 
 // Schema
 const planSchema = new Schema({
-    name: { type:String, required:true, index:true },
-    price: { type:Number },
-    description: { type:String },
-    features:[{ type:String }],
-    allowsUserAccess: { type:Boolean },
-    allowsBusinessAccess: { type:Boolean },
-    durationDays: { type:Number },
-    stripePriceId: { type:String, required:true }
+    name: { type: String, required: true, index: true },
+    price: { type: Number },
+    description: { type: String },
+    features:[{ type: String }],
+    allowsUserAccess: { type: Boolean },
+    allowsBusinessAccess: { type: Boolean },
+    durationDays: { type: Number },
+    stripePriceId: { type: String }
 });
 
 // Model

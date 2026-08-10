759d4594c9359a7a64d3712d0aad858442ba1cd9
Techforge25 <github@techforgeinnovations.com>
2026-08-10 12:52:55 +0600

    Update cron job timer

--- src/cron/subscriptionAutoExpire.js
+++ src/cron/subscriptionAutoExpire.js
@@ -1,8 +1,8 @@
 const cron = require("node-cron");
 const Subscription = require("../models/subscription");
 
-// Runs at (12:00 AM, 6:00 AM, 12:00 PM, 6:00 PM)
-const timer = "0 */12 * * *";
+// Runs daily at 12:00 AM, 6:00 AM, 12:00 PM, and 6:00 PM
+const timer = "0 0,6,12,18 * * *";
 
 // Auto exprire subscription
 cron.schedule(timer, async () => {

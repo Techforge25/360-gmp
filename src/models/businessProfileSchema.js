b9c410d80f7c082c397966bcb7f8c6d71e8dd2b9
Techforge25 <github@techforgeinnovations.com>
2026-08-10 14:24:07 +0600

    Update status

--- src/models/businessProfileSchema.js
+++ src/models/businessProfileSchema.js
@@ -128,7 +128,7 @@ const BusinessProfileSchema = new Schema({
     evidenceOfFunds: { type: String, trim: true, required: true },
 
     // Other
-    status: { type: String, enum:["pending", "approved", "rejected"], default: "approved" },
+    status: { type: String, enum:["pending", "approved", "rejected"], default: "pending" },
     isVerified: { type: Boolean, default: false },
     mapURL: { type:String },
     latitude: { type:Number, default:0 },

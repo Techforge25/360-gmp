ec4041ea2e295a0db2d6c126dd7e918f78202dbb
Techforge25 <github@techforgeinnovations.com>
2026-08-12 10:53:22 +0600

    Refactor code

--- src/routes/admin/reportManagementRoute.js
+++ src/routes/admin/reportManagementRoute.js
@@ -11,39 +11,30 @@ const reportManagementRouter = Router();
 reportManagementRouter.use(adminAuthentication, grantAccessTo("Reports"));
 
 // Fetch report stats
-reportManagementRouter.route("/stats")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), fetchReportStats);
+reportManagementRouter.route("/stats").get(fetchReportStats);
 
 // Fetch job reports
-reportManagementRouter.route("/job")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), fetchJobReports);
+reportManagementRouter.route("/job").get(fetchJobReports);
 
 // Fetch Business profile reports
-reportManagementRouter.route("/businessProfile")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), fetchBusinessProfileReports);
+reportManagementRouter.route("/businessProfile").get(fetchBusinessProfileReports);
 
 // Fetch product reports
-reportManagementRouter.route("/product")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), fetchProductReports);
+reportManagementRouter.route("/product").get(fetchProductReports);
 
 // Fetch community reports
-reportManagementRouter.route("/community")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), fetchCommunityReports);
+reportManagementRouter.route("/community").get(fetchCommunityReports);
 
 // View job report
-reportManagementRouter.route("/job/:reportId")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), viewJobReport);
+reportManagementRouter.route("/job/:reportId").get(viewJobReport);
 
 // View business report
-reportManagementRouter.route("/business/:reportId")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), viewBusinessReport);
+reportManagementRouter.route("/business/:reportId").get(viewBusinessReport);
 
 // View product report
-reportManagementRouter.route("/product/:reportId")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), viewProductReport);
+reportManagementRouter.route("/product/:reportId").get(viewProductReport);
 
 // View community report
-reportManagementRouter.route("/community/:reportId")
-.get(adminAuthentication, adminAuthorization(["superAdmin"]), viewCommunityReport);
+reportManagementRouter.route("/community/:reportId").get(viewCommunityReport);
 
 module.exports = reportManagementRouter;

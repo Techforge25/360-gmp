f95b7706ae1dc9269fd061d8b321a57fc884c539
Techforge25 <github@techforgeinnovations.com>
2026-08-18 11:18:33 +0600

    Exclude role

--- src/controllers/businessProfileController.js
+++ src/controllers/businessProfileController.js
@@ -46,7 +46,7 @@ const createBusinessProfile = asyncHandler(async (request, response) => {
             ownerId: profile._id, ownerModel: "BusinessProfile", 
             pendingBalance: 0, availableBalance: 0, totalEarned: 0, currency: 'USD' 
         }),
-        User.findByIdAndUpdate(userId, { role: "business", isNewToPlatform: false }, { new: true, lean: true })
+        User.findByIdAndUpdate(userId, { isNewToPlatform: false }, { new: true, lean: true })
     ]);
 
     // Validate

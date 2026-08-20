8bd42a590715996e6d3dd6adc392c185f428dbc6
Techforge25 <github@techforgeinnovations.com>
2026-08-20 10:48:19 +0600

    Add date filter

--- src/controllers/admin/communityManagementController.js
+++ src/controllers/admin/communityManagementController.js
@@ -9,6 +9,35 @@ const { emptyList } = require("../../constants");
 const { isValidObjectId } = require("mongoose");
 const convertToMongoId = require("../../utils/convertToMongoId");
 
+// Allowed date filters
+const allowedDateFilters = ["all", "1d", "3d", "7d"];
+
+// Helper function to implement date range
+const getDateFilter = (request, fieldName = "createdAt") => {
+    // Date filter
+    const { dateRange = "all" } = request.query;
+    if(!allowedDateFilters.includes(dateRange)) throw new ApiError(400, "Invalid date range");
+
+    // Date filter
+    const dateFilter = {};
+
+    if(dateRange !== "all")
+    {
+        // Calculate date
+        const now = new Date();
+        let startDate = new Date();
+
+        if(dateRange === "1d") startDate.setDate(now.getDate() - 1);
+        if(dateRange === "3d") startDate.setDate(now.getDate() - 3);
+        if(dateRange === "7d") startDate.setDate(now.getDate() - 7);
+
+        // Inject date range
+        dateFilter[fieldName] = { $gte: startDate };
+    }
+
+    return { dateFilter };
+};
+
 // Initiator
 const communityManagementInitiator = asyncHandler(async (request, response) => {
     // Response
@@ -251,10 +280,13 @@ const fetchCommunityPosts = asyncHandler(async (request, response) => {
     const { communityId } = request.params;
     if(!isValidObjectId(communityId)) throw new ApiError(400, "Invalid Community ID");
 
+    // Get date filter
+    const { dateFilter } = getDateFilter(request);
+
     // Fetch
     const posts = await CommunityPost.aggregatePaginate([
         // Match
-        { $match: { communityId: convertToMongoId(communityId) } }, 
+        { $match: { communityId: convertToMongoId(communityId), ...dateFilter } }, 
         
         // Lookup business profile
         {

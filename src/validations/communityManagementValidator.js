d274f50d57ec7925143267d510e66aeb4ea6f708
Techforge25 <github@techforgeinnovations.com>
2026-08-25 13:17:20 +0600

    Update: Make description field required in warn community owner validator

--- src/validations/communityManagementValidator.js
+++ src/validations/communityManagementValidator.js
@@ -3,7 +3,7 @@ const joi = require("joi");
 // Send warning to community validator
 const warnCommunityOwnerValidator = joi.object({
     reason: joi.string().trim().min(4).max(50).required().label("Reason"),
-    description: joi.string().trim().max(1000).optional().allow(null, "").label("Description")
+    description: joi.string().trim().min(4).max(1000).required().label("Description")
 });
 
 module.exports = { warnCommunityOwnerValidator };

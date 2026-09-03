8f55578ddf66b7854e801ec5b2183edc4b2b636c
Techforge25 <github@techforgeinnovations.com>
2026-09-03 18:36:41 +0600

    Feat: Create API to fetch active jobs

--- src/controllers/admin/jobManagementController.js
+++ src/controllers/admin/jobManagementController.js
@@ -37,4 +37,67 @@ const fetchJobStats = asyncHandler(async (request, response) => {
     return response.status(200).json(new ApiResponse(200, payload, "Job stats have been fetched"));
 });
 
-module.exports = { jobManagementInitiator, fetchJobStats };
+// Fetch active jobs
+const fetchActiveJobs = asyncHandler(async (request, response) => {
+    const { page = 1, limit = 10 } = request.query;
+
+    // Get date filter
+    const { dateFilter } = getDateFilter(request);    
+
+    // Fetch
+    const jobs = await Job.aggregatePaginate([
+        // Match
+        { $match: { status: "open", ...dateFilter } },
+
+        // Lookup business
+        {
+            $lookup: {
+                from: "businessprofiles",
+                localField: "businessId",
+                foreignField: "_id",
+                as: "businessProfile",
+                pipeline:[{ $project: { _id:0, companyName: 1, logo: 1 } }]
+            }
+        },
+
+        // Lookup job applications
+        {
+            $lookup: {
+                from: "jobapplications",
+                localField: "_id",
+                foreignField: "jobId",
+                as: "jobApplications"
+            }
+        },        
+
+        // Unwind
+        { $unwind: { path: "$businessProfile", preserveNullAndEmptyArrays: true } },
+
+        // Count total job applicants
+        {
+            $addFields: {
+                totalJobApplicants: { $size: "$jobApplications" }
+            }
+        },
+
+        // Sort
+        { $sort: { createdAt: -1 } },
+
+        // Project
+        {
+            $project: {
+                jobTitle: 1,
+                location: 1,
+                businessProfile: 1,
+                totalJobApplicants: 1,
+                createdAt: 1
+            }
+        },
+    ], { page, limit });
+    if(!jobs.totalDocs) return response.status(200).json(new ApiResponse(200, emptyList, "No active jobs found"));
+
+    // Response
+    return response.status(200).json(new ApiResponse(200, jobs, "Active jobs have been fetched"));
+});
+
+module.exports = { jobManagementInitiator, fetchJobStats, fetchActiveJobs };
--- src/routes/admin/jobManagementRoute.js
+++ src/routes/admin/jobManagementRoute.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { adminAuthentication, grantAccessTo } = require("../../middlewares/adminAuth");
-const { jobManagementInitiator, fetchJobStats } = require("../../controllers/admin/jobManagementController");
+const { jobManagementInitiator, fetchJobStats, fetchActiveJobs } = require("../../controllers/admin/jobManagementController");
 
 // Router instance
 const jobManagementRouter = Router();
@@ -13,5 +13,8 @@ jobManagementRouter.route("/init").get(jobManagementInitiator);
 
 // Fetch stats
 jobManagementRouter.route("/stats").get(fetchJobStats);
+
+// Fetch stats
+jobManagementRouter.route("/activeJobs").get(fetchActiveJobs);
 
 module.exports = jobManagementRouter;

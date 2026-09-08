d59a870b25e8a7dabef3510731da91c69a740f36
Techforge25 <github@techforgeinnovations.com>
2026-09-08 13:13:29 +0600

    Feat: Create API to fetch comments of a specific post with pagination metadata

--- src/controllers/postCommentController.js
+++ src/controllers/postCommentController.js
@@ -33,7 +33,7 @@ const createComment = asyncHandler(async (request, response) => {
     const post = await CommunityPost.findById(postId).select("-_id communityId");
     if(!post) throw new ApiError(404, "Post not found");
 
-    // Authorize to like or unlike post
+    // Authorize to comment
     const membership = await CommunityMembership.findOne({
         communityId: post.communityId, 
         memberId: commenterId,
@@ -64,4 +64,88 @@ const createComment = asyncHandler(async (request, response) => {
     return response.status(201).json(new ApiResponse(201, payload, "Comment has been submitted"));
 });
 
-module.exports = { createComment };
+// Fetch comments
+const fetchComments = asyncHandler(async (request, response) => {
+    const { page = 1, limit = 10 } = request.query;
+
+    // Sanitize ID
+    const { postId } = request.params;
+    if(!isValidObjectId(postId)) throw new ApiError(400, "Invalid Post ID");
+
+    // Get profile role and IDs
+    const { userProfileId, businessProfileId } = request.user.profiles || {};
+    const { role } = request.user;
+
+    // Fetch post
+    const post = await CommunityPost.findById(postId).select("-_id communityId");
+    if(!post) throw new ApiError(404, "Post not found");
+
+    // Set dynamic member ID and model
+    const memberId = role === "user" ? userProfileId : businessProfileId;
+    const memberModel = role === "user" ? "UserProfile" : "BusinessProfile";    
+
+    // Authorize to view comments
+    const membership = await CommunityMembership.findOne({
+        communityId: post.communityId,
+        memberId,
+        memberModel,
+        status: "approved"
+    });
+    if(!membership) throw new ApiError(403, "To view comments on this post, you need to be a member of this community");
+
+    // Fetch
+    const comments = await PostComment.aggregatePaginate([
+        // Match
+        { $match: { postId: convertToMongoId(postId) } },
+
+        // Lookup user profile
+        {
+            $lookup: {
+                from: "userprofiles",
+                localField: "commenterId",
+                foreignField: "_id",
+                as: "userProfile",
+                pipeline: [{ $project: { _id: 0, name: "$fullName", logo: 1 } }]
+            }
+        },          
+
+        // Lookup business profile
+        {
+            $lookup: {
+                from: "businessprofiles",
+                localField: "commenterId",
+                foreignField: "_id",
+                as: "businessProfile",
+                pipeline: [{ $project: { _id: 0, name: "$ownerName", logo: 1 } }]
+            }
+        },          
+        
+        // Unwind
+        { $unwind: { path: "$userProfile", preserveNullAndEmptyArrays: true } }, 
+        { $unwind: { path: "$businessProfile", preserveNullAndEmptyArrays: true } },
+
+        // Sort
+        { $sort: { createdAt: -1 } },
+
+        // Projection
+        {
+            $project: {
+                content: 1,
+                commentedBy: {
+                    $cond: [
+                        { $eq: ["$commenterModel", "UserProfile"] },
+                        "$userProfile",
+                        "$businessProfile"
+                    ]
+                },            
+                createdAt: 1
+            }
+        }
+    ], { page, limit });
+    if(!comments.totalDocs) return response.status(200).json(new ApiResponse(200, emptyList, "No comments found"));
+
+    // Response
+    return response.status(200).json(new ApiResponse(200, comments, "Comments have been fetched"));
+});
+
+module.exports = { createComment, fetchComments };
--- src/routes/postCommentRoute.js
+++ src/routes/postCommentRoute.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const { authentication, authorization } = require("../middlewares/auth");
 const limitRequest = require("../middlewares/rateLimit");
-const { createComment } = require("../controllers/postCommentController");
+const { createComment, fetchComments } = require("../controllers/postCommentController");
 
 // Router instance
 const postCommentRouter = Router();
 
-// Create comment
+// Create comment / Fetch comments
 postCommentRouter.route("/:postId")
-.post(authentication, authorization(["user", "business"]), limitRequest({ maxRequests: 10 }), createComment);
+.post(authentication, authorization(["user", "business"]), limitRequest({ maxRequests: 10 }), createComment)
+.get(authentication, authorization(["user", "business"]), fetchComments);
 
 module.exports = postCommentRouter;

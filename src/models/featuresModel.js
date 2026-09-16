8b90b1849b56ceab19b0b18e9f69a66e3c26261f
Techforge25 <github@techforgeinnovations.com>
2026-09-16 14:04:37 +0600

    Revamp featur schema

--- src/models/featuresModel.js
+++ src/models/featuresModel.js
@@ -1,10 +1,16 @@
 const { Schema, model } = require("mongoose");
+const { allowedFeatureNames } = require("../constants");
 
 // Schema
 const featureSchema = new Schema({
-    name: { type:String, trim:true, required:true, unique:true, },
-    description: { type:String, trim:true }
-}, { timestamps:true });
+    // Reference
+    planId: { type: Schema.Types.ObjectId, ref: "Plan" },
+
+    // Info
+    name: { type: String, required: true, enum: allowedFeatureNames, index: true, unique: true },
+    limit: { type: Number, default: 0 },
+    isInfinite: { type: Boolean, default: false }
+}, { timestamps: true });
 
 // Model
 const Feature = model("Feature", featureSchema);
